57b26dda3ab90f85f195c223f8d90da04e426ee4
kille72 <mail@exotic.se>
2017-01-31 22:29:49 +0100

    kernel-arm7: Fix access to unitialized pointer (ported from DD-WRT) (Broadcom bug)
    Picked from AsusWRT-Merlin: https://github.com/RMerl/asuswrt-merlin/commit/1f335df24a26bd80a34782bed7ca7ea8cb99ee56

--- release/src-rt-7.x.main/src/linux/linux-2.6.36/arch/arm/plat-brcm/nvram_linux.c
+++ release/src-rt-7.x.main/src/linux/linux-2.6.36/arch/arm/plat-brcm/nvram_linux.c
@@ -772,9 +772,9 @@ nvram_commit(void)
 		header = (struct nvram_header *)(buf + i);
 		magic_offset = i + ((void *)&header->magic - (void *)header);
 	} else {
-		offset = nvram_mtd->size - nvram_space;
-		magic_offset = ((void *)&header->magic - (void *)header);
+		offset = nvram_mtd->size - nvram_space;		
 		header = (struct nvram_header *)buf;
+		magic_offset = ((void *)&header->magic - (void *)header);
 	}
 
 	/* clear the existing magic # to mark the NVRAM as unusable 
