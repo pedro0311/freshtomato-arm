19480c5b2756dcd13c370cc781510fe0fbbd61b7
Michał Obrembski <michal.o@szerszen.com>
2017-01-31 19:01:22 +0100

    Fix buildbreak after merging shibby-arm7

--- release/src-rt-6.x.4708/router/dnscrypt/libltdl/config.h
+++ release/src-rt-6.x.4708/router/dnscrypt/libltdl/config.h
@@ -120,7 +120,7 @@
 /* #undef LTDL_DLOPEN_DEPLIBS */
 
 /* Define to the system default library search path. */
-#define LT_DLSEARCH_PATH "/lib:/usr/lib:/usr/lib/x86_64-linux-gnu/libfakeroot:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/lib/i586-linux-gnu:/usr/lib/i586-linux-gnu:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/lib32:/usr/lib32:/libx32:/usr/libx32"
+#define LT_DLSEARCH_PATH "/lib:/usr/lib:/usr/lib/x86_64-linux-gnu/libfakeroot:/lib/i386-linux-gnu:/usr/lib/i386-linux-gnu:/lib/i686-linux-gnu:/usr/lib/i686-linux-gnu:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/mesa-egl:/usr/lib/fglrx:/usr/lib32/fglrx:/lib32:/usr/lib32"
 
 /* The archive extension */
 #define LT_LIBEXT "a"
