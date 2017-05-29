8d1b8999371dabfa7423ffa97c4b077dee669dfe
pedro <pedro311@gmail.com>
2017-05-29 18:08:10 +0200

    rc/tor.c: removed deprecated option from config (AllowUnverifiedNodes)

--- release/src-rt-6.x.4708/router/rc/tor.c
+++ release/src-rt-6.x.4708/router/rc/tor.c
@@ -7,54 +7,48 @@
 #include <rc.h>
 #include <sys/stat.h>
 
-void start_tor(void)
-{
-    FILE *fp;
-    char *ip;
+void start_tor(void) {
+	FILE *fp;
+	char *ip;
 
-//  only if enable...
-    if( nvram_match( "tor_enable", "1" ) )
-    {
+	// only if enable...
+	if (nvram_match("tor_enable", "1")) {
 
-    if (nvram_match( "tor_iface", "br0" ) ) { ip = nvram_safe_get( "lan_ipaddr" ); }
-            else if (nvram_match( "tor_iface", "br1" ) ) { ip = nvram_safe_get( "lan1_ipaddr" ); }
-            else if (nvram_match( "tor_iface", "br2" ) ) { ip = nvram_safe_get( "lan2_ipaddr" ); }
-            else if (nvram_match( "tor_iface", "br3" ) ) { ip = nvram_safe_get( "lan3_ipaddr" ); }
-            else { ip = nvram_safe_get( "lan_ipaddr" ); }
+		if (nvram_match("tor_iface", "br0"))      { ip = nvram_safe_get("lan_ipaddr");  }
+		else if (nvram_match("tor_iface", "br1")) { ip = nvram_safe_get("lan1_ipaddr"); }
+		else if (nvram_match("tor_iface", "br2")) { ip = nvram_safe_get("lan2_ipaddr"); }
+		else if (nvram_match("tor_iface", "br3")) { ip = nvram_safe_get("lan3_ipaddr"); }
+		else                                      { ip = nvram_safe_get("lan_ipaddr");  }
 
 
-//      writing data to file
-        if( !( fp = fopen( "/etc/tor.conf", "w" ) ) )
-        {
-            perror( "/etc/tor.conf" );
-            return;
-        }
-            fprintf(fp, "SocksPort %d\n", nvram_get_int( "tor_socksport" ) );
-//            fprintf(fp, "SocksBindAddress 127.0.0.1\n");
-            fprintf(fp, "AllowUnverifiedNodes middle,rendezvous\n");
-            fprintf(fp, "RunAsDaemon 1\n");
-            fprintf(fp, "Log notice syslog\n");
-            fprintf(fp, "DataDirectory %s\n", nvram_safe_get( "tor_datadir" ) );
-            fprintf(fp, "TransPort %s:%s\n", ip, nvram_safe_get( "tor_transport" ) );
-            fprintf(fp, "DNSPort %s:%s\n", ip, nvram_safe_get( "tor_dnsport" ) );
-            fprintf(fp, "User nobody\n");
-            fprintf(fp, "%s\n", nvram_safe_get("tor_custom"));
+		// writing data to file
+		if(!(fp = fopen( "/etc/tor.conf", "w"))) {
+			perror("/etc/tor.conf");
+			return;
+		}
+		fprintf(fp, "SocksPort %d\n", nvram_get_int("tor_socksport"));
+		fprintf(fp, "RunAsDaemon 1\n");
+		fprintf(fp, "Log notice syslog\n");
+		fprintf(fp, "DataDirectory %s\n", nvram_safe_get("tor_datadir"));
+		fprintf(fp, "TransPort %s:%s\n", ip, nvram_safe_get("tor_transport"));
+		fprintf(fp, "DNSPort %s:%s\n", ip, nvram_safe_get("tor_dnsport"));
+		fprintf(fp, "User nobody\n");
+		fprintf(fp, "%s\n", nvram_safe_get("tor_custom"));
 
-    fclose( fp );
-    chmod( "/etc/tor.conf", 0644 );
-    chmod( "/dev/null", 0666 );
+		fclose(fp);
+		chmod("/etc/tor.conf", 0644);
+		chmod("/dev/null", 0666);
 
-    mkdir( nvram_safe_get("tor_datadir"), 0777 );
-    xstart( "chown", "nobody:nobody", nvram_safe_get("tor_datadir") );
+		mkdir(nvram_safe_get("tor_datadir"), 0777);
+		xstart("chown", "nobody:nobody", nvram_safe_get("tor_datadir"));
 
-    xstart( "tor", "-f", "/etc/tor.conf" );
-    }
+		xstart("tor", "-f", "/etc/tor.conf");
+	}
 
-    return;
+	return;
 }
 
-void stop_tor(void)
-{
-    killall("tor", SIGTERM);
-    return;
+void stop_tor(void) {
+	killall("tor", SIGTERM);
+	return;
 }
