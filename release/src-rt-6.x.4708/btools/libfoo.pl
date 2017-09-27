4966110766c5d9ca8a2bea3c0760ee474849cdda
AndreDVJ <andre@debian>
2017-09-27 10:48:14 -0300

    libs optimization

--- release/src-rt-6.x.4708/btools/libfoo.pl
+++ release/src-rt-6.x.4708/btools/libfoo.pl
@@ -528,7 +528,7 @@ genSO("${root}/usr/lib/libpng.so.3.57.0", "${router}/libpng/.libs/libpng.a");
 genSO("${root}/usr/lib/libpng12.so.0.57.0", "${router}/libpng/.libs/libpng12.a");
 genSO("${root}/usr/lib/libjpeg.so", "${router}/libjpeg/.libs/libjpeg.a");
 genSO("${root}/usr/lib/libxml2.so.2.9.3", "${router}/libxml2/.libs/libxml2.a");
-genSO("${root}/usr/lib/libipset.so.3.6.0", "${router}/ipset/lib/.libs/libipset.a");
+genSO("${root}/usr/lib/libipset.so.3.7.0", "${router}/ipset/lib/.libs/libipset.a");
 genSO("${root}/usr/lib/libpcre.so.1.2.9", "${router}/pcre/.libs/libpcre.a");
 genSO("${root}/usr/lib/libpcreposix.so.0.0.5", "${router}/pcre/.libs/libpcreposix.a");
 
