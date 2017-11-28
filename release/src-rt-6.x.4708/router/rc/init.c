0e1ca22d921e72bf46b031450c50cb2f4d058c32
pedro <pedro311@gmail.com>
2017-11-28 02:26:44 +0100

    rc: usb.c/init.c fixed old typos

--- release/src-rt-6.x.4708/router/rc/init.c
+++ release/src-rt-6.x.4708/router/rc/init.c
@@ -4568,7 +4568,7 @@ int init_main(int argc, char *argv[])
 			run_nvscript("script_shut", NULL, 10);
 
 			stop_services();
-			stop_wan("wan");
+			stop_wan();
 			stop_lan();
 			stop_vlan();
 			stop_syslog();
