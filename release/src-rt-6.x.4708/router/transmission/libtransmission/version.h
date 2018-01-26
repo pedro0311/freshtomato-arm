9d534e437d1195993c18c77119c4e87102a230dc
pedro <pedro311@gmail.com>
2018-01-26 09:56:22 +0100

    transmission: update to 2.93
    - clean sources
    - move file btcheck to patches subdir
    - fixed CVE-2018-5702 https://github.com/transmission/transmission/pull/468 !

--- release/src-rt-6.x.4708/router/transmission/libtransmission/version.h
+++ release/src-rt-6.x.4708/router/transmission/libtransmission/version.h
@@ -1,10 +1,12 @@
-#define PEERID_PREFIX             "-TR2920-"
-#define USERAGENT_PREFIX          "2.92"
-#define SVN_REVISION              "14714"
-#define SVN_REVISION_NUM          14714
-#define SHORT_VERSION_STRING      "2.92"
-#define LONG_VERSION_STRING       "2.92 (14714)"
-#define VERSION_STRING_INFOPLIST  2.92
+#pragma once
+
+#define PEERID_PREFIX             "-TR2930-"
+#define USERAGENT_PREFIX          "2.93"
+#define VCS_REVISION              "3c5870d4f5"
+#define VCS_REVISION_NUM          3c5870d4f5
+#define SHORT_VERSION_STRING      "2.93"
+#define LONG_VERSION_STRING       "2.93 (3c5870d4f5)"
+#define VERSION_STRING_INFOPLIST  2.93
 #define MAJOR_VERSION             2
-#define MINOR_VERSION             92
+#define MINOR_VERSION             93
 #define TR_STABLE_RELEASE         1
