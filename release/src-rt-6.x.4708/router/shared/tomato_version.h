0d119304e89e8b8ab8265c121125931c87068442
pedro <pedro311@gmail.com>
2018-02-09 18:26:49 +0100

    Bump version to 2018.1

--- release/src-rt-6.x.4708/router/shared/tomato_version.h
+++ release/src-rt-6.x.4708/router/shared/tomato_version.h
@@ -1,8 +1,8 @@
 #ifndef __TOMATO_VERSION_H__
 #define __TOMATO_VERSION_H__
-#define TOMATO_MAJOR		"2017"
-#define TOMATO_MINOR		"3"
-#define TOMATO_BUILD		"018"
+#define TOMATO_MAJOR		"2018"
+#define TOMATO_MINOR		"1"
+#define TOMATO_BUILD		"001"
 #define	TOMATO_BUILDTIME	"Thu, 26 Jan 2018 01:01:01 +0100"
 #define TOMATO_VERSION		"2017.3.018 -my_build- K26ARM USB AIO-64K"
 #define TOMATO_SHORTVER		"2017.3"
