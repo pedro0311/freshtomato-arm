59cd4b6a83ba4a9c55a744cb8b105a2b61f46fd9
pedro <pedro311@gmail.com>
2018-03-11 21:23:57 +0100

    entware: update installation script

--- release/src-rt-6.x.4708/router/others/entware-install.sh
+++ release/src-rt-6.x.4708/router/others/entware-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+unset LD_LIBRARY_PATH
+unset LD_PRELOAD
+
 echo "Info: Checking for prerequisites and creating folders..."
 
 if [ -d /opt ]
@@ -8,7 +11,8 @@ then
 else
     mkdir /opt
 fi
-for folder in bin etc/init.d lib/opkg sbin share tmp usr var/log var/lock var/run
+# no need to create many folders. entware-opt package creates most
+for folder in bin etc lib/opkg tmp var/lock
 do
   if [ -d "/opt/$folder" ]
   then
@@ -20,30 +24,67 @@ do
 done
 
 echo "Info: Opkg package manager deployment..."
-CURARCH="armv7"
 DLOADER="ld-linux.so.3"
-URL=http://pkg.entware.net/binaries/$CURARCH/installer
+URL=http://bin.entware.net/armv7sf-k2.6/installer
 wget $URL/opkg -O /opt/bin/opkg
-chmod +x /opt/bin/opkg
+chmod 755 /opt/bin/opkg
 wget $URL/opkg.conf -O /opt/etc/opkg.conf
 wget $URL/ld-2.23.so -O /opt/lib/ld-2.23.so
-wget $URL/libc-2.23.so -O/opt/lib/libc-2.23.so
-wget $URL/libgcc_s.so.1.2.23 -O /opt/lib/libgcc_s.so.1
+wget $URL/libc-2.23.so -O /opt/lib/libc-2.23.so
+wget $URL/libgcc_s.so.1 -O /opt/lib/libgcc_s.so.1
+wget $URL/libpthread-2.23.so -O /opt/lib/libpthread-2.23.so
 cd /opt/lib
-chmod +x ld-2.23.so
+chmod 755 ld-2.23.so
 ln -s ld-2.23.so $DLOADER
 ln -s libc-2.23.so libc.so.6
+ln -s libpthread-2.23.so libpthread.so.0
 
 echo "Info: Basic packages installation..."
 /opt/bin/opkg update
 /opt/bin/opkg install entware-opt
-if [ ! -f /opt/usr/lib/locale/locale-archive ]
+
+# Fix for multiuser environment
+chmod 777 /opt/tmp
+
+# now try create symlinks - it is a std installation
+if [ -f /etc/passwd ]
 then
-        wget http://pkg.entware.net/binaries/other/locale-archive.2.23 -O /opt/usr/lib/locale/locale-archive
+    ln -sf /etc/passwd /opt/etc/passwd
+else
+    cp /opt/etc/passwd.1 /opt/etc/passwd
+fi
+
+if [ -f /etc/group ]
+then
+    ln -sf /etc/group /opt/etc/group
+else
+    cp /opt/etc/group.1 /opt/etc/group
+fi
+
+if [ -f /etc/shells ]
+then
+    ln -sf /etc/shells /opt/etc/shells
+else
+    cp /opt/etc/shells.1 /opt/etc/shells
+fi
+
+if [ -f /etc/shadow ]
+then
+    ln -sf /etc/shadow /opt/etc/shadow
+fi
+
+if [ -f /etc/gshadow ]
+then
+    ln -sf /etc/gshadow /opt/etc/gshadow
+fi
+
+if [ -f /etc/localtime ]
+then
+    ln -sf /etc/localtime /opt/etc/localtime
 fi
 
 echo "Info: Congratulations!"
-echo "Info: If there are no errors above then Entware-ng was successfully initialized."
+echo "Info: If there are no errors above then Entware was successfully initialized."
 echo "Info: Add /opt/bin & /opt/sbin to your PATH variable"
-echo "Info: Add '/opt/etc/init.d/rc.unslung start' to startup script for Entware-ng services to start"
-echo "Info: Found a Bug? Please report at https://github.com/Entware-ng/Entware-ng/issues"
+echo "Info: Add '/opt/etc/init.d/rc.unslung start' to startup script for Entware services to start"
+echo "Info: Found a Bug? Please report at https://github.com/Entware/Entware/issues"
