b2ec22e57faf3443ba59c5dd0534eb4c30dfa461
kille72 <mail@exotic.se>
2018-05-10 15:31:51 +0200

    transmission: updated to 2.94

--- release/src-rt-6.x.4708/router/transmission/libtransmission/version.h
+++ release/src-rt-6.x.4708/router/transmission/libtransmission/version.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#define PEERID_PREFIX             "-TR2930-"
-#define USERAGENT_PREFIX          "2.93"
-#define VCS_REVISION              "3c5870d4f5"
-#define VCS_REVISION_NUM          3c5870d4f5
-#define SHORT_VERSION_STRING      "2.93"
-#define LONG_VERSION_STRING       "2.93 (3c5870d4f5)"
-#define VERSION_STRING_INFOPLIST  2.93
+#define PEERID_PREFIX             "-TR2940-"
+#define USERAGENT_PREFIX          "2.94"
+#define VCS_REVISION              "d8e60ee44f"
+#define VCS_REVISION_NUM          d8e60ee44f
+#define SHORT_VERSION_STRING      "2.94"
+#define LONG_VERSION_STRING       "2.94 (d8e60ee44f)"
+#define VERSION_STRING_INFOPLIST  2.94
+#define BUILD_STRING_INFOPLIST    14714.2.94
 #define MAJOR_VERSION             2
-#define MINOR_VERSION             93
+#define MINOR_VERSION             94
 #define TR_STABLE_RELEASE         1
