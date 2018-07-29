ae1b19226d76f9446fbc7c4bf3d2a899592d7da1
kille72 <mail@exotic.se>
2018-07-29 17:41:31 +0200

    php: updated to 7.2.8

--- release/src-rt-6.x.4708/router/php/ext/reflection/php_reflection.c
+++ release/src-rt-6.x.4708/router/php/ext/reflection/php_reflection.c
@@ -4496,7 +4496,7 @@ ZEND_METHOD(reflection_class, getConstants)
 	ZEND_HASH_FOREACH_STR_KEY_PTR(&ce->constants_table, key, c) {
 		if (UNEXPECTED(zval_update_constant_ex(&c->value, ce) != SUCCESS)) {
 			zend_array_destroy(Z_ARRVAL_P(return_value));
-			return;
+			RETURN_NULL();
 		}
 		val = zend_hash_add_new(Z_ARRVAL_P(return_value), key, &c->value);
 		Z_TRY_ADDREF_P(val);
@@ -5531,7 +5531,7 @@ ZEND_METHOD(reflection_property, getValue)
 			return;
 		}
 
-		if (!instanceof_function(Z_OBJCE_P(object), ref->ce)) {
+		if (!instanceof_function(Z_OBJCE_P(object), ref->prop.ce)) {
 			_DO_THROW("Given object is not an instance of the class this property was declared in");
 			/* Returns from this function */
 		}
