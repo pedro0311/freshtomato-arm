49a38e2e667692753ffc030bdde9c5fe075ac516
Michal Obrembski <michal.o@szerszen.com>
2018-10-17 19:01:38 +0200

    GUI: add feature to generate VPN static key from GUI

--- release/src-rt-6.x.4708/router/httpd/tomato.c
+++ release/src-rt-6.x.4708/router/httpd/tomato.c
@@ -316,6 +316,7 @@ const struct mime_handler mime_handlers[] = {
 	{ "shutdown.cgi",		mime_html,				0,	wi_generic,		wo_shutdown,		1 },
 #ifdef TCONFIG_OPENVPN
 	{ "vpnstatus.cgi",		mime_javascript,			0,	wi_generic,		wo_vpn_status,		1 },
+	{ "vpngenkey.cgi",		mime_javascript,			0,	wi_generic,		wo_vpn_genkey,		1 },
 #endif
 #ifdef TCONFIG_PPTPD
 	{ "pptpd.cgi",			mime_javascript,			0,	wi_generic,		wo_pptpdcmd,		1 },	//!!AB - PPTPD
