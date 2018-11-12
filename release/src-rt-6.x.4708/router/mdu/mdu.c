42123be9d55594284a0a8f553ff1095ff020807e
pedro <pedro311@gmail.com>
2018-11-12 11:19:58 +0100

    mdu/mssl: add TLS SNI support
    - ported from Merlin: https://github.com/RMerl/asuswrt-merlin.ng/commit/a35c5775e502e9169837b8cacd4fd0814325fe95

--- release/src-rt-6.x.4708/router/mdu/mdu.c
+++ release/src-rt-6.x.4708/router/mdu/mdu.c
@@ -37,7 +37,7 @@
 #ifdef DEBUG
 #define AGENT		"MDU - TEST DDNS CLIENT"
 #else
-#define AGENT		"MDU - Tomato Firmware " TOMATO_MAJOR "." TOMATO_MINOR
+#define AGENT		"MDU - FreshTomato Firmware " TOMATO_MAJOR "." TOMATO_MINOR
 #endif
 
 #define MAX_OPTION_LENGTH	256
@@ -288,7 +288,7 @@ static int _wget(int ssl, const char *host, int port, const char *request, char 
 
 		if (ssl) {
 			mssl_init(NULL, NULL);
-			f = ssl_client_fopen(sd);
+			f = ssl_client_fopen_name(sd, host);
 		}
 		else {
 			f = fdopen(sd, "r+");
