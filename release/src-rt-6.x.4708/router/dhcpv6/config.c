5a48cd214550901f56b4325fd560095de23033b0
M_ars <M_ars@linksysinfo.org>
2018-11-29 21:42:01 +0100

    WIDE-DHCPv6: Adding ifid option to the dhcp6c.conf prefix-interface statement
    This adds the ability to override the default EUI-64 address selection.
    Useful for interfaces without a unique hardware address, or for creating more userfriendly addresses in general.
    
    see https://src.fedoraproject.org/rpms/wide-dhcpv6/tree/master

--- release/src-rt-6.x.4708/router/dhcpv6/config.c
+++ release/src-rt-6.x.4708/router/dhcpv6/config.c
@@ -448,6 +448,7 @@ add_pd_pif(iapdc, cfl0, if_count)
 {
 	struct cf_list *cfl;
 	struct prefix_ifconf *pif;
+	int i;
 
 	/* duplication check */
 	for (pif = TAILQ_FIRST(&iapdc->iapd_pif_list); pif;
@@ -501,6 +502,10 @@ add_pd_pif(iapdc, cfl0, if_count)
 				    configfilename, cfl->line, pif->sla_len); 
 				goto bad;
 			}
+			break;
+		case IFPARAM_IFID:
+			for (i = sizeof(pif->ifid) -1; i >= 0; i--)
+				pif->ifid[i] = (cfl->num >> 8*(sizeof(pif->ifid) - 1 - i)) & 0xff;
 			break;
 		default:
 			dprintf(LOG_ERR, FNAME, "%s:%d internal error: "
