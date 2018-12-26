2a41cab856b04570969e1ac510653fcff3a5cb96
pedro <pedro311@gmail.com>
2018-12-26 23:19:15 +0100

    router: rc: tomatoanon.c: cosmetics
    - stay as close as possible to MIPS version

--- release/src-rt-6.x.4708/router/rc/tomatoanon.c
+++ release/src-rt-6.x.4708/router/rc/tomatoanon.c
@@ -9,23 +9,21 @@
 
 void start_tomatoanon(void)
 {
+	/* only if enable... */
+	if ((!nvram_match("tomatoanon_enable", "1")) || (!nvram_match("tomatoanon_answer", "1")))
+		return;
 
-//  only if enable...
-    if (!nvram_match("tomatoanon_enable", "1")) return;
-    if (!nvram_match("tomatoanon_answer", "1")) return;
+	xstart( "tomatoanon" );
 
-    xstart( "tomatoanon" );
+	if (nvram_match("tomatoanon_notify", "1"))
+		xstart( "tomatoanon", "checkver" );
 
-    if (nvram_match("tomatoanon_notify", "1"))
-         xstart( "tomatoanon", "checkver" );
-
-    return;
+	return;
 }
 
 void stop_tomatoanon(void)
 {
-    xstart( "cru", "d", "anonupdate" );
-    xstart( "cru", "d", "checkver" );
-
-    return;
+	xstart( "cru", "d", "anonupdate" );
+	xstart( "cru", "d", "checkver" );
+	return;
 }
