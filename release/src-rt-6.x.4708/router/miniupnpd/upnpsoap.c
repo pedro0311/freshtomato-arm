794460d84d17abe8ab7d0310ee4ae49eb197a85a
kille72 <mail@exotic.se>
2019-02-16 12:53:19 +0100

    miniupnpd: Updated to 2.1.20190210

--- release/src-rt-6.x.4708/router/miniupnpd/upnpsoap.c
+++ release/src-rt-6.x.4708/router/miniupnpd/upnpsoap.c
@@ -1,4 +1,4 @@
-/* $Id: upnpsoap.c,v 1.152 2018/07/06 12:05:48 nanard Exp $ */
+/* $Id: upnpsoap.c,v 1.153 2019/02/10 11:47:11 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
