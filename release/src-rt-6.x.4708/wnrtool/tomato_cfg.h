0c2fb23a979ddfa625e93ef735d6dfee9cf2ac91
snowman58 <mrdon213@gmail.com>
2019-04-13 22:34:38 -0600

    Raise revision level to allow initial files install from stock NETGEAR

---
+++ release/src-rt-6.x.4708/wnrtool/tomato_cfg.h
@@ -0,0 +1,22 @@
+/* Define Firmware Versions */
+#define WW_VERSION      1
+#define NA_VERSION      2
+#define JP_VERSION      3
+#define GR_VERSION      4
+#define PR_VERSION      5
+#define KO_VERSION      6
+#define RU_VERSION      7
+#define SS_VERSION      8
+#define PT_VERSION      9
+#define TWC_VERSION    10
+#define BRIC_VERSION   11
+#define SK_VERSION     12
+
+/* Define Firmware Regions as World Wide Version */
+#define WLAN_REGION   WW_VERSION
+#define FW_REGION     WW_VERSION
+
+/* Set Firmware Version */
+#define AMBIT_SOFTWARE_VERSION     "V9.0.0.0"
+#define AMBIT_UI_VERSION           "9.0.0"
+
