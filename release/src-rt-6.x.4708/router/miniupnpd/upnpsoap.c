5fc8c6abbe7f99af1e99f76c63adabaabdb0d1a7
kille72 <mail@exotic.se>
2019-04-14 10:16:09 +0200

    miniupnpd: Updated to 2.1.20190408
    - Fix memory leak in upnpreplyparse.c with NewPortListing element

--- release/src-rt-6.x.4708/router/miniupnpd/upnpsoap.c
+++ release/src-rt-6.x.4708/router/miniupnpd/upnpsoap.c
@@ -1,8 +1,8 @@
-/* $Id: upnpsoap.c,v 1.154 2019/04/03 16:25:54 nanard Exp $ */
+/* $Id: upnpsoap.c,v 1.155 2019/04/08 13:31:46 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * MiniUPnP project
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
- * (c) 2006-2018 Thomas Bernard
+ * (c) 2006-2019 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 
@@ -521,6 +521,7 @@ AddPortMapping(struct upnphttp * h, const char * action, const char * ns)
 	/* possible error codes for AddPortMapping :
 	 * 402 - Invalid Args
 	 * 501 - Action Failed
+	 * 606 - Action not authorized (added in IGD v2)
 	 * 715 - Wildcard not permited in SrcAddr
 	 * 716 - Wildcard not permited in ExtPort
 	 * 718 - ConflictInMappingEntry
@@ -537,7 +538,8 @@ AddPortMapping(struct upnphttp * h, const char * action, const char * ns)
      * 728 - NoPortMapsAvailable
              There are not enough free ports available to complete the mapping
              (added in IGD v2)
-	 * 729 - ConflictWithOtherMechanisms (added in IGD v2) */
+	 * 729 - ConflictWithOtherMechanisms (added in IGD v2)
+	 * 732 - WildCardNotPermittedInIntPort (added in IGD v2) */
 	switch(r)
 	{
 	case 0:	/* success */
@@ -550,8 +552,12 @@ AddPortMapping(struct upnphttp * h, const char * action, const char * ns)
 		SoapError(h, 729, "ConflictWithOtherMechanisms");
 		break;
 #endif /* IGD_V2 */
-	case -2:	/* already redirected */
 	case -3:	/* not permitted */
+#ifdef IGD_V2
+		SoapError(h, 606, "Action not authorized");
+		break;
+#endif /* IGD_V2 */
+	case -2:	/* already redirected */
 		SoapError(h, 718, "ConflictInMappingEntry");
 		break;
 	default:
