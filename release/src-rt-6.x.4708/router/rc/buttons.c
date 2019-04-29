fc19cfcd963d3df996c714f698115db9bb0e9168
M_ars <M_ars@linksysinfo.org>
2019-04-29 20:58:33 +0200

    RT-AC68U: add wifi button - reset button gpio 11 active LOW (no change) - wps button gpio 7 active LOW (no change) - wifi button gpio 15 active LOW (new/change)
    Use LED AOSS (Power LED) for feedback if a button is pushed
    Hint: wifi button does only toggle WLAN on/off

--- release/src-rt-6.x.4708/router/rc/buttons.c
+++ release/src-rt-6.x.4708/router/rc/buttons.c
@@ -193,6 +193,11 @@ int buttons_main(int argc, char *argv[])
 		ses_led = LED_AOSS;
 		break;
 	case MODEL_RTAC68U:
+		reset_mask = 1 << 11; /* reset button (active LOW) */
+		ses_mask = 1 << 7; /* wps button (active LOW) */
+		wlan_mask = 1 << 15;  /* wifi button (active LOW) */
+		ses_led = LED_AOSS;
+		break;
 	case MODEL_DIR868L:
 		reset_mask = 1 << 11;
 		ses_mask = 1 << 7;
