f1a36f54ab184c87eaefc3db7f535e8afd8521d8
pedro <pedro311@gmail.com>
2019-05-21 13:25:27 +0200

    router: Remove unused variables
    - httpd: tomato.c, shared: defaults.c

--- release/src-rt-6.x.4708/router/httpd/tomato.c
+++ release/src-rt-6.x.4708/router/httpd/tomato.c
@@ -1570,8 +1570,6 @@ static const nvset_t nvset_list[] = {
 	{ "vpn_server1_key",		V_NONE				},
 	{ "vpn_server1_dh",		V_NONE				},
 	{ "vpn_server1_br",		V_LENGTH(0, 50)			},
-	{ "vpn_server1_route",		V_01				},
-	{ "vpn_server1_routing_val",	V_NONE				},
 	{ "vpn_server2_poll",		V_RANGE(0, 30)			},
 	{ "vpn_server2_if",		V_TEXT(3, 3)			},	// tap, tun
 	{ "vpn_server2_proto",		V_TEXT(3, 11)			},	// udp, tcp-server, udp4, tcp4-server, udp6, tcp6-server
@@ -1613,8 +1611,6 @@ static const nvset_t nvset_list[] = {
 	{ "vpn_server2_key",		V_NONE				},
 	{ "vpn_server2_dh",		V_NONE				},
 	{ "vpn_server2_br",		V_LENGTH(0, 50)			},
-	{ "vpn_server2_route",		V_01				},
-	{ "vpn_server2_routing_val",	V_NONE				},
 	{ "vpn_client_eas",		V_NONE				},
 	{ "vpn_client1_poll",		V_RANGE(0, 30)			},
 	{ "vpn_client1_if",		V_TEXT(3, 3)			},	// tap, tun
