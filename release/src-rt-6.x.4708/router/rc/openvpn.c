ec09e63dc0fd4bd25716b092f3af69868bf2b99e
notsure2 <notsure2@protonmail.com>
2019-06-22 14:10:38 +0200

    router/rc/openvpn.c: Make firewall rules consistent in both IPv4 and IPv6.

--- release/src-rt-6.x.4708/router/rc/openvpn.c
+++ release/src-rt-6.x.4708/router/rc/openvpn.c
@@ -446,7 +446,7 @@ void start_ovpn_client(int clientNum)
 
 		sprintf(buffer, "vpn_client%d_fw", clientNum);
 		nvi = nvram_get_int(buffer);
-		fprintf(fp, "iptables -I INPUT -i %s -j %s\n", iface, (nvi ? "DROP" : "ACCEPT"));
+		fprintf(fp, "iptables -I INPUT -i %s -m state --state NEW -j %s\n", iface, (nvi ? "DROP" : "ACCEPT"));
 		fprintf(fp, "iptables -I FORWARD -i %s -m state --state NEW -j %s\n", iface, (nvi ? "DROP" : "ACCEPT"));
 		fprintf(fp, "iptables -I FORWARD -o %s -j ACCEPT\n", iface);
 
@@ -477,6 +477,7 @@ void start_ovpn_client(int clientNum)
 		if (ipv6_enabled()) {
 			fprintf(fp, "ip6tables -I INPUT -i %s -m state --state NEW -j %s\n", iface, (nvi ? "DROP" : "ACCEPT"));
 			fprintf(fp, "ip6tables -I FORWARD -i %s -m state --state NEW -j %s\n", iface, (nvi ? "DROP" : "ACCEPT"));
+			fprintf(fp, "ip6tables -I FORWARD -o %s -j ACCEPT\n", iface);
 		}
 #endif
 
