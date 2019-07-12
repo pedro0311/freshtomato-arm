b1b3c7bd027ed7f06fbebf23e81a60431c54c707
M_ars <M_ars@linksysinfo.org>
2019-07-12 19:48:04 +0200

    DIR868L / WS880: set boot_wait=on and set wait_time=3
    Hint: Wait time can be adjusted at GUI, see advanced-misc.asp; (Valid range: 3 to 20 sec)
    Delay a bit before loading Linux/Tomato, to allow SW updates (for example);

--- release/src-rt-6.x.4708/router/rc/init.c
+++ release/src-rt-6.x.4708/router/rc/init.c
@@ -2407,6 +2407,10 @@ static int init_nvram(void)
 			nvram_set("ehci_ports", "2-1 2-2");
 			nvram_set("ohci_ports", "3-1 3-2");
 
+			/* misc settings */
+			nvram_set("boot_wait", "on");
+			nvram_set("wait_time", "3");
+
 			/* force wl1 settings */
 			nvram_set("wl1_bw", "3");
 			nvram_set("wl1_bw_cap", "7");
@@ -2691,8 +2695,8 @@ static int init_nvram(void)
 			nvram_set("ohci_ports", "3-1 3-2");
 
 			/* misc settings */
-			nvram_set("boot_wait", "off");
-			nvram_set("wait_time", "1");
+			nvram_set("boot_wait", "on");
+			nvram_set("wait_time", "3");
 
 			/* 2.4GHz module defaults */
 			nvram_set("devpath0", "pci/1/1");
