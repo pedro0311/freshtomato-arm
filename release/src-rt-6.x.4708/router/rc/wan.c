64180033fcbd79445fb72643509dc39949de1a12
notsure2 <notsure2@protonmail.com>
2019-08-17 10:32:50 +0200

    Fix race condition starting wireless WAN causing route addition failure.
    - This is due to start_wan starting wan interfaces then immediately restarting
      wireless services. Any wan interfaces that are wireless and dhcp will be in a race
      between the dhcpc bound event and the wireless service restart causing random
      failure of route addition.
    - This was tested on MIPS with a Linksys E900 with Wireless DHCP WAN.

--- release/src-rt-6.x.4708/router/rc/wan.c
+++ release/src-rt-6.x.4708/router/rc/wan.c
@@ -1026,6 +1026,10 @@ void start_wan(int mode)
 		mwan_num = 1;
 	}
 
+	syslog(LOG_INFO, "start_wan: Restarting wireless");
+	stop_wireless();
+	start_wireless();
+
 	syslog(LOG_INFO, "MultiWAN: MWAN is %d (max %d)", mwan_num, MWAN_MAX);
 
 	for (wan_unit = 1; wan_unit <= mwan_num; ++wan_unit) {
@@ -1034,8 +1038,6 @@ void start_wan(int mode)
 		start_wan_if (mode, prefix);
 	}
 
-	stop_wireless();
-	start_wireless();
 	start_firewall();
 	set_host_domain_name();
 
