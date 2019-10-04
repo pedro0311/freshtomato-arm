pedro0311/freshtomato-arm

commit 2b1294a41364b5c6d60f5fdbdadde079c97cb605

Tenda AC15: use variable model to determine Tenda AC15 (and add some more infos)
thx for testing & feedback to Marcin1108

release/src-rt-6.x.4708/router/shared/id.c
@@ -78,6 +78,7 @@ ZTE H618B			HW_BCM5354G           0x048e     1105        0x35      0x0750
 Tenda N60                      BCM47186              0x052B       60        0x1400    0x00000710 //8MB/64MB/2.4/5G/USB
 Tenda N6                       BCM5357               0x0550       6         0x1444    0x710 //8MB/64MB/2.4/5G/USB
 TENDA W1800R                   HW_BCM4706            0x05d8       18/21(EU)/60(CN)   0x1200  0x00000110
+Tenda AC15			BCM4708               0x0646       30        0x1100 //model=AC15V1.0
 Tenda AC18			BCM4708               0x0646       30        0x1100 //model=AC18V1.0
 
 TrendNET			BCM4708               0x0646       1234      0x1100    0x80001200
@@ -384,7 +385,7 @@ int get_model(void)
 		if ((nvram_match("boardtype","0xF646")) && (nvram_match("boardnum", "01"))) return MODEL_EA6700;
 		if ((nvram_match("boardtype","0xF646")) && (nvram_match("boardnum", "00"))) return MODEL_WZR1750;
 		if ((nvram_match("boardtype","0xD646")) && (nvram_match("boardrev", "0x1100"))) return MODEL_EA6900;
-		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("1:boardnum", "AC155g"))) return MODEL_AC15;//Fallback to Tenda AC15
+		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("model", "AC15V1.0"))) return MODEL_AC15; /* Tenda AC15 */
 		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("model", "AC18V1.0"))) return MODEL_AC18; /* Tenda AC18 */
 	}
 #endif

commit 5ca351d8c77e6de9d03142f57bca8849072afe35

Tenda AC15: correct/fix detection, caused by commit https://bitbucket.org/kille72/freshtomato-arm/commits/2b1294a41364b5c6d60f5fdbdadde079c97cb605
There are at least 2 different hw/cfe versions from Tenda for that model. With this change, all known versions are correctly detected (again)
See also:
https://www.linksysinfo.org/index.php?threads/fork-freshtomato-arm.74117/page-149#post-308062

Thx to Techie007 for helping & testing

release/src-rt-6.x.4708/router/shared/id.c
@@ -26,7 +26,7 @@ EA6350v1	    		BCM47081A0            0xE646       20140309  0x1200    0x00000110
 
 WZR-1750DHP	    		BCM4708               0xF646       00        0x1100    0x0110      0:devid=0x4332
 
-Tenda AC15			BCM4708               0x0646       30        0x1100 //model=AC15V1.0
+Tenda AC15			BCM4708               0x0646       30        0x1100 //1:boardnum=AC155g  HINT: model=AC15V1.0 only available for some HW-Versions, so at least 2 different cfe´s from Tenda!
 Tenda AC18			BCM4708               0x0646       30        0x1100 //model=AC18V1.0
 
 TrendNET			BCM4708               0x0646       1234      0x1100    0x80001200
@@ -96,7 +96,7 @@ int get_model(void)
 		if ((nvram_match("boardtype","0xF646")) && (nvram_match("boardnum", "01"))) return MODEL_EA6700;
 		if ((nvram_match("boardtype","0xF646")) && (nvram_match("boardnum", "00"))) return MODEL_WZR1750;
 		if ((nvram_match("boardtype","0xD646")) && (nvram_match("boardrev", "0x1100"))) return MODEL_EA6900;
-		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("model", "AC15V1.0"))) return MODEL_AC15; /* Tenda AC15 */
+		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("1:boardnum", "AC155g"))) return MODEL_AC15; /* Tenda AC15 */
 		if ((nvram_match("boardrev", "0x1100")) && (nvram_match("model", "AC18V1.0"))) return MODEL_AC18; /* Tenda AC18 */
 	}
 #endif /* CONFIG_BCMWL6A */