7ff750cadbd7901641084a66f218f424bccc01c1
M_ars <M_ars@gmx.net>
2020-02-22 11:04:26 +0100

    rc: network.c - rework start and stop of emf/lan/wl - fix/correct start and stop of EMF (stop failed almost every time and also router stuck/hung sometimes at reboot via GUI!) - make EMF multi-lan aware - give feedback about start and stop EMF - rework basic start and stop of start_lan / start_lan_wl / start_wl / start_wireless ... - cosmetic - set start_wan(BOOT) last one --> at services.c (align also to init.c)
    tested with RT-N18U and R7000, working (reboot, restart, wifi bridge, VIFs, multi lan, wifi on/off, et cetera... all ok)
    
    HINT: prepare arm-ng branch for merge with arm-master (add missing router)

--- release/src-rt-6.x.4708/router/rc/services.c
+++ release/src-rt-6.x.4708/router/rc/services.c
@@ -3150,7 +3150,6 @@ TOP:
 			start_vlan();
 			start_lan();
 			start_arpbind();
-			start_wan(BOOT);
 			start_nas();
 			start_dnsmasq();
 			start_httpd();
@@ -3158,6 +3157,10 @@ TOP:
 #ifdef TCONFIG_USB
 			start_nas_services();
 #endif
+			/*
+			 * last one as ssh telnet httpd samba etc can fail to load until start_wan_done
+			 */
+			start_wan(BOOT);
 		}
 		goto CLEAR;
 	}
@@ -3167,7 +3170,7 @@ TOP:
 			stop_wireless();
 		}
 		if (act_start) {
-			start_wireless();
+			restart_wireless();
 		}
 		goto CLEAR;
 	}
@@ -3175,13 +3178,9 @@ TOP:
 	if (strcmp(service, "wl") == 0) {
 		if (act_stop) {
 			stop_wireless();
-			unload_wl();
 		}
 		if (act_start) {
-			load_wl();
-			start_wireless();
-			stop_wireless();
-			start_wireless();
+			restart_wireless();
 		}
 		goto CLEAR;
 	}
