088bee213fae754e84c052b6ce448e7e309bfb3d
M_ars <M_ars@gmx.net>
2020-02-26 16:47:27 +0100

    rc: init.c - remove start_nas()/stop_nas() (already done at start_services()/stop_services())

--- release/src-rt-6.x.4708/router/rc/init.c
+++ release/src-rt-6.x.4708/router/rc/init.c
@@ -4621,7 +4621,6 @@ int init_main(int argc, char *argv[])
 			run_nvscript("script_shut", NULL, 10);
 
 			stop_services();
-			stop_nas();
 			stop_wan();
 			stop_arpbind();
 			stop_lan();
@@ -4675,7 +4674,6 @@ int init_main(int argc, char *argv[])
 			start_vlan();
 			start_lan();
 			start_arpbind();
-			start_nas();
 			mwan_state_files();
 			start_services();
 
