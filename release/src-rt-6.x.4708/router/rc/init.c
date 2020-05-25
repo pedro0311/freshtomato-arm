b5599bd347a149a7ceec15843339d2fa5a12be29
M_ars <M_ars@gmx.net>
2020-05-25 11:03:02 +0200

    R6400v1: do not enable vhtmode and vht_features for 2G wifi module
    Note: prevent/avoid problems on older/cost optimized/partly NON-AC hardware.

--- release/src-rt-6.x.4708/router/rc/init.c
+++ release/src-rt-6.x.4708/router/rc/init.c
@@ -1557,6 +1557,9 @@ static int init_nvram(void)
 			nvram_set("wl1_nbw_cap","3");
 			nvram_set("wl1_nctrlsb", "lower");
 
+			/* misc wifi settings */
+			nvram_set("wl0_vreqd", "0"); /* do not enable vhtmode and vht_features for 2G NON-AC PHY */
+
 			/* 2.4 GHz defaults */
 			nvram_set("pci/1/1/aa2g", "7");
 			nvram_set("pci/1/1/ag0", "0");
