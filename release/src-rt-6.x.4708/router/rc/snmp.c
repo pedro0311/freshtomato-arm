592db826a7c80a6001b4d4b33138d23355b34ea8
pedro <pedro311@gmail.com>
2020-08-17 20:44:58 +0200

    router: rc: snmp.c: code optimization and clean-up. One version for ARM and MIPS branch.
    - in PRs please use #ifdef TCONFIG_BCMARM/#ifndef TCONFIG_BCMARM/etc to distinguish different branches

--- release/src-rt-6.x.4708/router/rc/snmp.c
+++ release/src-rt-6.x.4708/router/rc/snmp.c
@@ -4,8 +4,12 @@
  * Copyright (C) 2011 shibby
  *
  */
+
 #include <rc.h>
 #include <sys/stat.h>
+
+#define SNMP_CONF	"/etc/snmpd.conf"
+
 
 void start_snmp(void)
 {
@@ -14,36 +18,33 @@ void start_snmp(void)
 	/*  only if enabled... */
 	if (nvram_match("snmp_enable", "1")) {
 		/* writing data to file */
-		if (!(fp = fopen("/etc/snmpd.conf", "w"))) {
-			perror("/etc/snmpd.conf");
+		if (!(fp = fopen(SNMP_CONF, "w"))) {
+			perror(SNMP_CONF);
 			return;
 		}
-		fprintf(fp,
-			"agentaddress udp:%d\n"
-			"syslocation %s\n"
-			"syscontact %s <%s>\n"
-			"rocommunity %s\n"
-			"extend device /bin/echo \"%s\"\n"
-			"extend version /bin/echo \"FreshTomato %s\"\n",
-			nvram_get_int("snmp_port"),
-			nvram_safe_get("snmp_location"),
-			nvram_safe_get("snmp_contact"),
-			nvram_safe_get("snmp_contact"),
-			nvram_safe_get("snmp_ro"),
-			nvram_safe_get("t_model_name"),
-			nvram_safe_get("os_version"));
+		fprintf(fp, "agentaddress udp:%d\n"
+		            "syslocation %s\n"
+		            "syscontact %s <%s>\n"
+		            "rocommunity %s\n"
+		            "extend device /bin/echo \"%s\"\n"
+		            "extend version /bin/echo \"FreshTomato %s\"\n",
+		            nvram_get_int("snmp_port"),
+		            nvram_safe_get("snmp_location"),
+		            nvram_safe_get("snmp_contact"),
+		            nvram_safe_get("snmp_contact"),
+		            nvram_safe_get("snmp_ro"),
+		            nvram_safe_get("t_model_name"),
+		            nvram_safe_get("os_version"));
+
 		fclose(fp);
 
-		chmod("/etc/snmpd.conf", 0644);
+		chmod(SNMP_CONF, 0644);
 
-		xstart("snmpd", "-c", "/etc/snmpd.conf");
+		xstart("snmpd", "-c", SNMP_CONF);
 	}
-
-	return;
 }
 
 void stop_snmp(void)
 {
 	killall("snmpd", SIGTERM);
-	return;
 }
