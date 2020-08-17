817ab21695ceeb965c682995aee0176bcabac59a
pedro <pedro311@gmail.com>
2020-08-17 20:39:04 +0200

    router: rc: tomatoanon.c: code optimization and clean-up. One version for ARM and MIPS branch.
    - in PRs please use #ifdef TCONFIG_BCMARM/#ifndef TCONFIG_BCMARM/etc to distinguish different branches

--- release/src-rt-6.x.4708/router/rc/tomatoanon.c
+++ release/src-rt-6.x.4708/router/rc/tomatoanon.c
@@ -4,8 +4,10 @@
  * Copyright (C) 2012 shibby
  *
  */
+
 #include <rc.h>
 #include <sys/stat.h>
+
 
 void start_tomatoanon(void)
 {
@@ -13,17 +15,14 @@ void start_tomatoanon(void)
 	if ((!nvram_match("tomatoanon_enable", "1")) || (!nvram_match("tomatoanon_answer", "1")))
 		return;
 
-	xstart( "tomatoanon" );
+	xstart("tomatoanon");
 
 	if (nvram_match("tomatoanon_notify", "1"))
-		xstart( "tomatoanon", "checkver" );
-
-	return;
+		xstart("tomatoanon", "checkver");
 }
 
 void stop_tomatoanon(void)
 {
-	xstart( "cru", "d", "anonupdate" );
-	xstart( "cru", "d", "checkver" );
-	return;
+	xstart("cru", "d", "anonupdate");
+	xstart("cru", "d", "checkver");
 }
