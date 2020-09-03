dab7acc7d0e82c7034bc0f69c552fdd069d220e1
pedro <pedro311@gmail.com>
2020-09-03 10:49:05 +0200

    kernel sdk6: drivers: net: usb: updates from upstream
    - usbnet: export usbnet_set_rx_mode()
    https://github.com/torvalds/linux/commit/1ea2b748b5eb4f9b6c8f66d098f1277afb724336
    
    - cdc_ether: add new products ids
    
    - cdc_ether: use dev->intf to get interface information
    https://github.com/torvalds/linux/commit/0226009ce0f6089f9b31211f7a2703cf9a327a01
    
    - cdc_ether: export usbnet_cdc_update_filter
    https://github.com/torvalds/linux/commit/e506addeff844237d60545ef4f6141de21471caf
    
    - cdc_ncm: add .ndo_set_rx_mode to cdc_ncm_netdev_ops
    https://github.com/torvalds/linux/commit/37a2ebdd9e597ae1a0270ac747883ea8f6f767b6
    
    - cdc_ncm: hook into set_rx_mode to admit multicast traffic
    https://github.com/torvalds/linux/commit/e10dcb1b6ba714243ad5a35a11b91cc14103a9a9

--- release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/usb/cdc_ncm.c
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/usb/cdc_ncm.c
@@ -796,6 +796,7 @@ static const struct net_device_ops cdc_ncm_netdev_ops = {
 	.ndo_stop	     = usbnet_stop,
 	.ndo_start_xmit	     = usbnet_start_xmit,
 	.ndo_tx_timeout	     = usbnet_tx_timeout,
+	.ndo_set_rx_mode     = usbnet_set_rx_mode,
 	.ndo_change_mtu	     = cdc_ncm_change_mtu,
 	.ndo_set_mac_address = eth_mac_addr,
 	.ndo_validate_addr   = eth_validate_addr,
@@ -1897,6 +1898,7 @@ static const struct driver_info cdc_ncm_info = {
 	.status = cdc_ncm_status,
 	.rx_fixup = cdc_ncm_rx_fixup,
 	.tx_fixup = cdc_ncm_tx_fixup,
+	.set_rx_mode = usbnet_cdc_update_filter,
 };
 
 /* Same as cdc_ncm_info, but with FLAG_WWAN */
@@ -1910,6 +1912,7 @@ static const struct driver_info wwan_info = {
 	.status = cdc_ncm_status,
 	.rx_fixup = cdc_ncm_rx_fixup,
 	.tx_fixup = cdc_ncm_tx_fixup,
+	.set_rx_mode = usbnet_cdc_update_filter,
 };
 
 /* Same as wwan_info, but with FLAG_NOARP  */
@@ -1923,6 +1926,7 @@ static const struct driver_info wwan_noarp_info = {
 	.status = cdc_ncm_status,
 	.rx_fixup = cdc_ncm_rx_fixup,
 	.tx_fixup = cdc_ncm_tx_fixup,
+	.set_rx_mode = usbnet_cdc_update_filter,
 };
 
 static const struct usb_device_id cdc_devs[] = {
