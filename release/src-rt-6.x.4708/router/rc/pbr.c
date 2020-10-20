866c7d0e022c5c4140bc504315e00562c784659d
pedro <pedro311@gmail.com>
2020-10-20 20:48:37 +0200

    rc: pbr.c: code optimization and clean-up. One version for ARM and MIPS branch.
    - in PRs please use #ifdef TCONFIG_BCMARM/#ifndef TCONFIG_BCMARM/etc to distinguish different branches

--- release/src-rt-6.x.4708/router/rc/pbr.c
+++ release/src-rt-6.x.4708/router/rc/pbr.c
@@ -6,12 +6,15 @@
 
 */
 
+
 #include "rc.h"
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
-void ipt_routerpolicy(void){
+
+void ipt_routerpolicy(void)
+{
 	char *nv, *nvp, *b;
 	char *active, *proto, *srt_type, *srt_addr, *srt_port, *dst_type, *dst_addr, *dst_port, *wanx, *desc;
 	char msrt[192],mdst[192],jump[16];
@@ -53,38 +56,31 @@ void ipt_routerpolicy(void){
 	*/
 
 	mwan_num = nvram_get_int("mwan_num");
-	if((mwan_num == 1 || mwan_num > MWAN_MAX)) return;
+	if ((mwan_num == 1 || mwan_num > MWAN_MAX))
+		return;
 
-	for(wan_unit = 1 ; wan_unit <= mwan_num; ++wan_unit){
+	for (wan_unit = 1; wan_unit <= mwan_num; ++wan_unit) {
 		get_wan_prefix(wan_unit, prefix);
-		if(check_wanup(prefix)){
-			ipt_write(
-				":WAN_%d - [0:0]\n"
-				"-A WAN_%d -m conntrack --ctstate NEW -j CONNMARK --set-return 0x%d00/0xf00\n"
-				"-A PREROUTING -i %s -j WAN_%d\n"
-				"-A POSTROUTING -o %s -j WAN_%d\n",
-				wan_unit,
-				wan_unit,
-				wan_unit,
-				get_wanface(prefix),
-				wan_unit,
-				get_wanface(prefix),
-				wan_unit
-				);
-		}
+		if (check_wanup(prefix))
+			ipt_write(":WAN_%d - [0:0]\n"
+			          "-A WAN_%d -m conntrack --ctstate NEW -j CONNMARK --set-return 0x%d00/0xf00\n"
+			          "-A PREROUTING -i %s -j WAN_%d\n"
+			          "-A POSTROUTING -o %s -j WAN_%d\n",
+			          wan_unit,
+			          wan_unit, wan_unit,
+			          get_wanface(prefix), wan_unit,
+			          get_wanface(prefix), wan_unit);
 	}
 
-	ipt_write(
-		":WAN_PBR - [0:0]\n"
-		"-A WAN_PBR -m state --state RELATED,ESTABLISHED -j RETURN\n"
-		"-A PREROUTING -i br+ -j WAN_PBR\n"
-		"-A PREROUTING -i br+ -m conntrack --ctstate ESTABLISHED,RELATED -j CONNMARK --restore-mark\n"
-		"-A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j CONNMARK --restore-mark\n"
-		);
+	ipt_write(":WAN_PBR - [0:0]\n"
+	          "-A WAN_PBR -m state --state RELATED,ESTABLISHED -j RETURN\n"
+	          "-A PREROUTING -i br+ -j WAN_PBR\n"
+	          "-A PREROUTING -i br+ -m conntrack --ctstate ESTABLISHED,RELATED -j CONNMARK --restore-mark\n"
+	          "-A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j CONNMARK --restore-mark\n");
 
 	nv = nvp = strdup(nvram_safe_get("pbr_rules"));
 
-	if(nv) {
+	if (nv) {
 		while ((b = strsep(&nvp, ">")) != NULL) {
 			/*
 			active<proto<srt_type<srt_addr<srt_port<dst_type<dst_addr<dst_port<wanx<desc
@@ -120,142 +116,125 @@ void ipt_routerpolicy(void){
 
 			1<-2<0<<<1<220.249.92.18<<1<1
 			*/
-			if ((vstrsep(b, "<", &active, &proto, &srt_type, &srt_addr, &srt_port, &dst_type, &dst_addr, &dst_port, &wanx, &desc)!=10)) continue;
+			if ((vstrsep(b, "<", &active, &proto, &srt_type, &srt_addr, &srt_port, &dst_type, &dst_addr, &dst_port, &wanx, &desc)!=10))
+				continue;
 
-			// active is not 1, drop the rule
-			if (*active != '1') continue;
+			/* active is not 1, drop the rule */
+			if (*active != '1')
+				continue;
 
 			memset(msrt, 0, sizeof(msrt));
-			if(atoi(srt_type) == 1){
+			if (atoi(srt_type) == 1)
 				sprintf(msrt, "-s %s", srt_addr);
-			}
-			else if(atoi(srt_type) == 2){
+			else if (atoi(srt_type) == 2)
 				sprintf(msrt, "-m mac --mac-source %s", srt_addr);
-			}
 
 			memset(jump, 0, sizeof(jump));
-			if(atoi(wanx) >= 1 && atoi(wanx) <= 4){
-				// wanup check fail, drop the rule
+			if (atoi(wanx) >= 1 && atoi(wanx) <= 4) {
+				/* wanup check fail, drop the rule */
 				get_wan_prefix(atoi(wanx), prefix);
-				if(!check_wanup(prefix)) continue;
+				if (!check_wanup(prefix))
+					continue;
+
 				sprintf(jump, "WAN_%s", wanx);
 			}
-			else{
+			else
 				continue;
-			}
 
-			if(atoi(dst_type) == 3){
+			if (atoi(dst_type) == 3) {
 				int i;
 				struct hostent *he;
 				struct in_addr **addr_list;
-				if ((he = gethostbyname(dst_addr)) == NULL) continue;
+				if ((he = gethostbyname(dst_addr)) == NULL)
+					continue;
 
 				addr_list = (struct in_addr **)he->h_addr_list;
 
 				i = 0;
-				while(addr_list[i] != NULL){
+				while (addr_list[i] != NULL) {
 					memset(mdst, 0, sizeof(mdst));
 					sprintf(mdst, "-d %s", inet_ntoa(*addr_list[i]));
 
 					msport = (strchr(srt_port, ',') != NULL) ? " -m multiport --sports " : " --sport ";
 					mdport = (strchr(dst_port, ',') != NULL) ? " -m multiport --dports " : " --dport ";
 
-					// protocol & ports
+					/* protocol & ports */
 					proto_num = atoi(proto);
 					if (proto_num > -2) {
 						if ((proto_num == 6) || (proto_num == 17) || (proto_num == -1)) {
-							if (proto_num != 6){
+							if (proto_num != 6)
 								ipt_write("-A WAN_PBR -p %s %s%s%s %s%s%s -j %s\n",
-									"udp",
-									msrt,
-									(*srt_port) ? msport : "",
-									(*srt_port) ? srt_port : "",
-									mdst,
-									(*dst_port) ? mdport : "",
-									(*dst_port) ? dst_port : "",
-									jump);
-							}
-							if (proto_num != 17) {
+								          "udp",
+								          msrt,
+								          (*srt_port) ? msport : "",
+								          (*srt_port) ? srt_port : "",
+								          mdst,
+								          (*dst_port) ? mdport : "",
+								          (*dst_port) ? dst_port : "",
+								          jump);
+							if (proto_num != 17)
 								ipt_write("-A WAN_PBR -p %s %s%s%s %s%s%s -j %s\n",
-									"tcp",
-									msrt,
-									(*srt_port) ? msport : "",
-									(*srt_port) ? srt_port : "",
-									mdst,
-									(*dst_port) ? mdport : "",
-									(*dst_port) ? dst_port : "",
-									jump);
-							}
+								          "tcp",
+								          msrt,
+								          (*srt_port) ? msport : "",
+								          (*srt_port) ? srt_port : "",
+								          mdst,
+								          (*dst_port) ? mdport : "",
+								          (*dst_port) ? dst_port : "",
+								          jump);
 						}
-						else {
+						else
 							ipt_write("-A WAN_PBR -p %s %s %s -j %s\n",
-								proto_num,
-								msrt,
-								mdst,
-								jump);
-						}
+							          proto_num, msrt, mdst, jump);
 					}
-					else {	// any protocol
+					else {	/* any protocol */
 						ipt_write("-A WAN_PBR %s %s -j %s\n",
-							msrt,
-							mdst,
-							jump);
+						          msrt, mdst, jump);
 					}
 					i++;
 				}
 			}
-			else
-			{
+			else {
 				memset(mdst, 0, sizeof(mdst));
-				if(atoi(dst_type) != 0){
+				if (atoi(dst_type) != 0)
 					sprintf(mdst, "-d %s", dst_addr);
-				}
 
 				msport = (strchr(srt_port, ',') != NULL) ? " -m multiport --sports " : " --sport ";
 				mdport = (strchr(dst_port, ',') != NULL) ? " -m multiport --dports " : " --dport ";
 
-
-				// protocol & ports
+				/* protocol & ports */
 				proto_num = atoi(proto);
 				if (proto_num > -2) {
 					if ((proto_num == 6) || (proto_num == 17) || (proto_num == -1)) {
-						if (proto_num != 6){
+						if (proto_num != 6)
 							ipt_write("-A WAN_PBR -p %s %s%s%s %s%s%s -j %s\n",
-								"udp",
-								msrt,
-								(*srt_port) ? msport : "",
-								(*srt_port) ? srt_port : "",
-								mdst,
-								(*dst_port) ? mdport : "",
-								(*dst_port) ? dst_port : "",
-								jump);
-						}
-						if (proto_num != 17) {
+							          "udp",
+							          msrt,
+							          (*srt_port) ? msport : "",
+							          (*srt_port) ? srt_port : "",
+							          mdst,
+							          (*dst_port) ? mdport : "",
+							          (*dst_port) ? dst_port : "",
+							          jump);
+						if (proto_num != 17)
 							ipt_write("-A WAN_PBR -p %s %s%s%s %s%s%s -j %s\n",
-								"tcp",
-								msrt,
-								(*srt_port) ? msport : "",
-								(*srt_port) ? srt_port : "",
-								mdst,
-								(*dst_port) ? mdport : "",
-								(*dst_port) ? dst_port : "",
-								jump);
-						}
+							          "tcp",
+							          msrt,
+							          (*srt_port) ? msport : "",
+							          (*srt_port) ? srt_port : "",
+							          mdst,
+							          (*dst_port) ? mdport : "",
+							          (*dst_port) ? dst_port : "",
+							          jump);
 					}
-					else {
+					else
 						ipt_write("-A WAN_PBR -p %s %s %s -j %s\n",
-							proto_num,
-							msrt,
-							mdst,
+						          proto_num, msrt, mdst,
 							jump);
-					}
 				}
-				else {	// any protocol
+				else	/* any protocol */
 					ipt_write("-A WAN_PBR %s %s -j %s\n",
-						msrt,
-						mdst,
-						jump);
-				}
+					          msrt, mdst, jump);
 			}
 		}
 	}
