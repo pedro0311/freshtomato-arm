5fb6c9d4dd0c032282f6692f00d25fa991d220a0
pedro <pedro311@gmail.com>
2020-10-21 10:53:24 +0200

    rc: sched.c: code optimization and clean-up. One version for ARM and MIPS branch.
    - in PRs please use #ifdef TCONFIG_BCMARM/#ifndef TCONFIG_BCMARM/etc to distinguish different branches

--- release/src-rt-6.x.4708/router/rc/sched.c
+++ release/src-rt-6.x.4708/router/rc/sched.c
@@ -5,17 +5,13 @@
 
 */
 
+
 #include <time.h>
 #include "rc.h"
 
 
-//	#define DLOG(args...) syslog(LOG_DEBUG, args)
-#define DLOG(fmt, args...) _dprintf(fmt"\n", args)
-
 static inline void unsched(const char *key)
 {
-	DLOG("%s: %s", __FUNCTION__, key);
-
 	eval("cru", "d", (char *) key);
 }
 
@@ -30,69 +26,70 @@ static void sched(const char *key, int resched)
 	struct tm tm;
 	long tt, qq;
 
-	// en,time,days
+	/* en,time,days */
 	if ((sscanf(nvram_safe_get(key), "%d,%d,%d", &en, &t, &dow) != 3) || (!en)) {
 		unsched(key);
 		return;
 	}
 
-	DLOG("%s: %s", __FUNCTION__, key);
-
 	if (resched) {
+		memset(s, 0 , 64);
 		sprintf(s, "%s_last", key);
+		memset(w, 0 , 32);
 		sprintf(w, "%ld", time(0));
 		nvram_set(s, w);
 
-		DLOG("%s: set %s=%s", __FUNCTION__, s, w);
-
-		if (t >= -5) return;
+		if (t >= -5)
+			return;
 	}
 
-	if ((dow & 0x7F) == 0) dow = 0x7F;
+	if ((dow & 0x7F) == 0)
+		dow = 0x7F;
+
 	w[0] = 0;
 	w[1] = 0;
 	for (i = 0; i < 7; ++i) {
-		if (dow & (1 << i)) {
+		if (dow & (1 << i))
 			sprintf(w + strlen(w), ",%d", i);
-		}
 	}
 
-	if (t >= 0) {	// specific time
+	if (t >= 0) { /* specific time */
+		memset(s, 0 , 64);
 		sprintf(s, "%d %d * * %s sched %s", t % 60, t / 60, w + 1, key);
 	}
-	else {	// every ...
+	else { /* every ... */
 		t = -t;
-		if (t <= 5) {	// 1 to 5m = a simple cron job
+		if (t <= 5) { /* 1 to 5m = a simple cron job */
+			memset(s, 0 , 64);
 			sprintf(s, "*/%d * * * %s sched %s", t, w + 1, key);
 		}
 		else {
 			t *= 60;
-			DLOG("%s: t=%d", __FUNCTION__, t);
 
 			tt = time(0) + 59;
 			tm = *localtime(&tt);
-			DLOG("%s: now=%d:%02d %02d/%02d", __FUNCTION__, tm.tm_hour, tm.tm_min, tm.tm_mon + 1, tm.tm_mday);
 
+			memset(s, 0 , 64);
 			sprintf(s, "%s_last", key);
-			DLOG("%s: %s_last=%s, tt=%ld", __FUNCTION__, key, nvram_safe_get(s), tt);
 			qq = strtoul(nvram_safe_get(s), NULL, 10);
-			if ((qq + t) > tt) tt = qq;
+			if ((qq + t) > tt)
+				tt = qq;
 
 			tt += t;
 			while (1) {
-				tm = *localtime(&tt);	// copy struct, otherwise we get weird stuff (!?)
-				DLOG("%s: %d:%02d %02d/%02d, tt=%ld", __FUNCTION__, tm.tm_hour, tm.tm_min, tm.tm_mon + 1, tm.tm_mday, tt);
-//				printf("%d:%02d %02d/%02d, w=%d, dow=%d, x=%d, tt=%ld\n", tm.tm_hour, tm.tm_min, tm.tm_mon + 1, tm.tm_mday, tm.tm_wday,
-//					dow, (dow & (1 << tm.tm_wday)), tt);
-				if (dow & (1 << tm.tm_wday)) break;
+				tm = *localtime(&tt); /* copy struct, otherwise we get weird stuff (!?) */
+
+				if (dow & (1 << tm.tm_wday))
+					break;
+
 				tt += 60;
 			}
 
+			memset(s, 0 , 64);
 			sprintf(s, "%d %d %d %d * sched %s", tm.tm_min, tm.tm_hour, tm.tm_mday, tm.tm_mon + 1, key);
 		}
 	}
 
-	DLOG("%s: cru a %s %s", __FUNCTION__, key, s);
 	eval("cru", "a", (char *) key, s);
 }
 
@@ -108,8 +105,6 @@ int sched_main(int argc, char *argv[])
 	int log;
 
 	if (argc == 2) {
-		DLOG("%s: %s", __FUNCTION__, argv[1]);
-
 		log = nvram_contains_word("log_events", "sched");
 
 		if (strncmp(argv[1], "sch_", 4) == 0) {
@@ -117,13 +112,17 @@ int sched_main(int argc, char *argv[])
 
 			if (is_sched(argv[1])) {
 				if (strcmp(argv[1], "sch_rboot") == 0) {
-					if (log) syslog(LOG_INFO, "Performing scheduled %s...", "reboot");
+					if (log)
+						syslog(LOG_INFO, "Performing scheduled reboot...");
+
 					eval("reboot");
 					return 0;
 				}
 				else if (strcmp(argv[1], "sch_rcon") == 0) {
 					sched(argv[1], 1);
-					if (log) syslog(LOG_INFO, "Performing scheduled %s...", "reconnect");
+					if (log)
+						syslog(LOG_INFO, "Performing scheduled reconnect...");
+
 					eval("service", "wan", "restart");
 				}
 				else if (strncmp(argv[1], "sch_c", 5) == 0) {
@@ -131,22 +130,19 @@ int sched_main(int argc, char *argv[])
 					if ((n >= 1) && (n <= 5)) {
 						sched(argv[1], 1);
 
-						if (log) {
-							sprintf(s, "custom #%d", n);
-							syslog(LOG_INFO, "Performing scheduled %s...", s);
-						}
+						if (log)
+							syslog(LOG_INFO, "Performing scheduled custom #%d...", n);
 
 						signal(SIGCHLD, chld_reap);
 
+						memset(s, 0 , 64);
 						sprintf(s, "%s_cmd", argv[1]);
-						DLOG("%s: run=%s", __FUNCTION__, nvram_safe_get(s));
 						run_nvscript(s, "", 60);
 					}
 				}
 			}
-			else {
+			else
 				unsched(argv[1]);
-			}
 		}
 		else if (strcmp(argv[1], "start") == 0) {
 			while (time(0) < Y2K) {
@@ -168,17 +164,15 @@ int sched_main(int argc, char *argv[])
 
 void start_sched(void)
 {
-	DLOG("%s", __FUNCTION__);
-
 	killall("sched", SIGTERM);
+
 	xstart("sched", "start");
 }
 
 void stop_sched(void)
 {
-	DLOG("%s", __FUNCTION__);
-
 	killall("sched", SIGTERM);
+
 	unsched("sch_rboot");
 	unsched("sch_rcon");
 	unsched("sch_sc1");
