24f7f959cbbdfdd2c42b0cf84a1ee1788e7714a8
pedro <pedro311@gmail.com>
2020-10-21 10:40:27 +0200

    rc: forward.c: code optimization and clean-up. One version for ARM and MIPS branch.
    - in PRs please use #ifdef TCONFIG_BCMARM/#ifndef TCONFIG_BCMARM/etc to distinguish different branches

--- release/src-rt-6.x.4708/router/rc/forward.c
+++ release/src-rt-6.x.4708/router/rc/forward.c
@@ -5,6 +5,7 @@
 
 */
 
+
 #include "rc.h"
 
 #include <arpa/inet.h>
@@ -12,6 +13,7 @@
 extern char chain_wan_prerouting[];
 
 static const char tcpudp[2][4] = {"tcp", "udp"};
+
 
 void ipt_forward(ipt_table_t table)
 {
@@ -24,28 +26,29 @@ void ipt_forward(ipt_table_t table)
 	char src[64];
 
 	nvp = nv = strdup(nvram_safe_get("portforward"));
-	if (!nv) return;
+	if (!nv)
+		return;
 
 	while ((b = strsep(&nvp, ">")) != NULL) {
 		/*
-			[<1.01] 1<3<30,40-45<60<5<desc
-			[<1.07] 1<3<30,40-45<60<192.168.1.5<desc
-
-			1<3<71.72.73.74<30,40-45<60<192.168.1.5<desc
-
-			1 = enabled
-			3 = tcp & udp
-			71.72.73.74 = src addr
-			30,40-45 = ext port
-			60 = int port
-			192.168.1.5 = dst addr
-			desc = desc
-
-		*/
+		 * [<1.01] 1<3<30,40-45<60<5<desc
+		 * [<1.07] 1<3<30,40-45<60<192.168.1.5<desc
+		 *
+		 * 1<3<71.72.73.74<30,40-45<60<192.168.1.5<desc
+		 *
+		 * 1 = enabled
+		 * 3 = tcp & udp
+		 * 71.72.73.74 = src addr
+		 * 30,40-45 = ext port
+		 * 60 = int port
+		 * 192.168.1.5 = dst addr
+		 * desc = desc
+		 */
 		n = vstrsep(b, "<", &c, &proto, &saddr, &xports, &iport, &iaddr, &desc);
-		if ((n < 6) || (*c != '1')) continue;
+		if ((n < 6) || (*c != '1'))
+			continue;
 		if (n == 6) {
-			// <1.07
+			/* <1.07 */
 			desc = iaddr;
 			iaddr = iport;
 			iport = xports;
@@ -53,11 +56,13 @@ void ipt_forward(ipt_table_t table)
 			saddr = "";
 		}
 
+		memset(src, 0 ,64);
 		if (!ipt_addr(src, sizeof(src), saddr, "src", IPT_V4, 1, "IPv4 port forwarding", desc))
 			continue;
 
+		memset(ip, 0, 64);
 		if (strchr(iaddr, '.') == NULL && strtol(iaddr, NULL, 10) > 0) {
-			// < 1.01: 5 -> 192.168.1.5
+			/* < 1.01: 5 -> 192.168.1.5 */
 			strcpy(ip, lan_cclass);
 			strlcat(ip, iaddr, sizeof(ip));
 		}
@@ -75,35 +80,33 @@ void ipt_forward(ipt_table_t table)
 				c = tcpudp[i];
 				if (table == IPT_TABLE_NAT) {
 					ipt_write("-A %s -p %s %s %s %s -j DNAT --to-destination %s%s%s\n",
-						chain_wan_prerouting,
-						c,
-						src,
-						mdport, xports,
-						ip,  *iport ? ":" : "", iport);
+					          chain_wan_prerouting,
+					          c,
+					          src,
+					          mdport, xports,
+					          ip,  *iport ? ":" : "", iport);
 
 					if (nvram_get_int("nf_loopback") == 1) {
 						for (n = 0; n < wanfaces.count; ++n) {
-							if (*(wanfaces.iface[n].name)) {
+							if (*(wanfaces.iface[n].name))
 								ipt_write("-A POSTROUTING -p %s %s %s -s %s/%s -d %s -j SNAT --to-source %s\n",
-									c,
-									mdport, *iport ? iport : xports,
-									nvram_safe_get("lan_ipaddr"),	// corrected by ipt
-									nvram_safe_get("lan_netmask"),
-									ip,
-									wanfaces.iface[n].ip);
-							}
+								          c,
+								          mdport, *iport ? iport : xports,
+								          nvram_safe_get("lan_ipaddr"), /* corrected by ipt */
+								          nvram_safe_get("lan_netmask"),
+								          ip,
+								          wanfaces.iface[n].ip);
 						}
 					}
 				}
-				else {	// filter
+				else /* filter */
 					ipt_write("-A wanin %s -p %s -m %s -d %s %s %s -j %s\n",
-						src,
-						c,
-						c,
-						ip,
-						mdport, *iport ? iport : xports,
-						chain_in_accept);
-				}
+					          src,
+					          c,
+					          c,
+					          ip,
+					          mdport, *iport ? iport : xports,
+					          chain_in_accept);
 			}
 		}
 	}
@@ -121,42 +124,49 @@ void ipt_triggered(ipt_table_t table)
 	char s[256];
 
 	nvp = nv = strdup(nvram_safe_get("trigforward"));
-	if (!nv) return;
+	if (!nv)
+		return;
 
 	first = 1;
 	while ((b = strsep(&nvp, ">")) != NULL) {
-		if ((vstrsep(b, "<", &c, &proto, &mports, &fports) != 4) || (*c != '1')) continue;
+		if ((vstrsep(b, "<", &c, &proto, &mports, &fports) != 4) || (*c != '1'))
+			continue;
+
 		for (i = 0; i < 2; ++i) {
 			if ((1 << i) & (*proto - '0')) {
 				if (first) {
-					// should only be created if there is at least one enabled
-
+					/* should only be created if there is at least one enabled */
 					if (table == IPT_TABLE_NAT) {
 						ipt_write("-A %s -j TRIGGER --trigger-type dnat\n", chain_wan_prerouting);
-						goto QUIT;
+						goto exit;
 					}
 
 					ipt_write(":triggers - [0:0]\n"
-							  "-A wanout -j triggers\n"
-							  "-A wanin -j TRIGGER --trigger-type in\n");
+					          "-A wanout -j triggers\n"
+					          "-A wanin -j TRIGGER --trigger-type in\n");
 
 					first = 0;
 				}
+				memset(s, 0, 256);
 				strlcpy(s, mports, sizeof(s));
-				if ((p = strchr(s, ':')) != NULL) *p = '-';
-				if ((p = strchr(fports, ':')) != NULL) *p = '-';
+				if ((p = strchr(s, ':')) != NULL)
+					*p = '-';
+				if ((p = strchr(fports, ':')) != NULL)
+					*p = '-';
+
 				c = tcpudp[i];
 				ipt_write("-A triggers -p %s -m %s --dport %s "
-						  "-j TRIGGER --trigger-type out --trigger-proto %s --trigger-match %s --trigger-relate %s\n",
-							c, c, mports,
-							c, s, fports);
-				// can't use multiport... trigger-match must be set to the same
-				// ports as dport since it's used to refresh timer during inbound	-- zzz
+				          "-j TRIGGER --trigger-type out --trigger-proto %s --trigger-match %s --trigger-relate %s\n",
+				          c, c, mports,
+				          c, s, fports);
+				/* can't use multiport... trigger-match must be set to the same
+				 * ports as dport since it's used to refresh timer during inbound
+				 */
 			}
 		}
 	}
 
-QUIT:
+exit:
 	free(nv);
 }
 
@@ -172,20 +182,22 @@ void ip6t_forward(void)
 	char dst[128];
 
 	nvp = nv = strdup(nvram_safe_get("ipv6_portforward"));
-	if (!nv) return;
+	if (!nv)
+		return;
 
 	while ((b = strsep(&nvp, ">")) != NULL) {
 		/*
-			1<3<2001:23:45:67::/64<2600:abc:def:123::1<30,40-45<desc
-
-			1 = enabled
-			3 = tcp & udp
-			2001:23:45:67::/64 = src addr
-			2600:abc:def:123::1 or 2001:10:11:12::/112 = dst addr (optional)
-			30,40-45 = dst port
-			desc = desc
-		*/
-		if ((vstrsep(b, "<", &c, &proto, &saddr, &daddr, &dports, &desc) != 6) || (*c != '1')) continue;
+		 * 1<3<2001:23:45:67::/64<2600:abc:def:123::1<30,40-45<desc
+		 *
+		 * 1 = enabled
+		 * 3 = tcp & udp
+		 * 2001:23:45:67::/64 = src addr
+		 * 2600:abc:def:123::1 or 2001:10:11:12::/112 = dst addr (optional)
+		 * 30,40-45 = dst port
+		 * desc = desc
+		 */
+		if ((vstrsep(b, "<", &c, &proto, &saddr, &daddr, &dports, &desc) != 6) || (*c != '1'))
+			continue;
 
 		if (!ipt_addr(src, sizeof(src), saddr, "src", IPT_V6, 1, "IPv6 port forwarding", desc))
 			continue;
@@ -198,12 +210,12 @@ void ip6t_forward(void)
 			if ((1 << i) & (*proto - '0')) {
 				c = tcpudp[i];
 				ip6t_write("-A wanin %s -p %s -m %s %s%s %s %s -j %s\n",
-					src,
-					c,
-					c,
-					(*daddr) ? "-d " : "", daddr,
-					mdport, dports,
-					chain_in_accept);
+				           src,
+				           c,
+				           c,
+				           (*daddr) ? "-d " : "", daddr,
+				           mdport, dports,
+				           chain_in_accept);
 			}
 		}
 	}
