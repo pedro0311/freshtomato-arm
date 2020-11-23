48ff90b7a458e7ecbfe93f5eb66a59e4d2fa398e
pedro <pedro311@gmail.com>
2020-11-23 21:37:42 +0100

    watchdog: set the default value for connection checker depending on whether or not there is curl in the image

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -72,7 +72,11 @@ struct nvram_tuple router_defaults[] = {
 	{ "wan_weight",			"1"				, 0 },
 	{ "wan_hilink_ip",		"0.0.0.0"			, 0 },
 	{ "wan_status_script",		"0"				, 0 },
+#ifdef TCONFIG_BBT
 	{ "wan_ckmtd",			"3"				, 0 },
+#else
+	{ "wan_ckmtd",			"2"				, 0 },
+#endif
 
 	{ "wan2_proto",			"dhcp"				, 0 },	// [static|dhcp|pppoe|disabled]
 	{ "wan2_ipaddr",		"0.0.0.0"			, 0 },	// WAN IP address
@@ -85,7 +89,11 @@ struct nvram_tuple router_defaults[] = {
 	{ "wan2_ifnameX",		NULL				, 0 },	// real wan if; see wan.c:start_wan
 	{ "wan2_hilink_ip",		"0.0.0.0"			, 0 },
 	{ "wan2_status_script",		"0"				, 0 },
+#ifdef TCONFIG_BBT
 	{ "wan2_ckmtd",			"3"				, 0 },
+#else
+	{ "wan2_ckmtd",			"2"				, 0 },
+#endif
 
 #ifdef TCONFIG_MULTIWAN
 	{ "wan3_proto",			"dhcp"				, 0 },	// [static|dhcp|pppoe|disabled]
@@ -99,7 +107,11 @@ struct nvram_tuple router_defaults[] = {
 	{ "wan3_ifnameX",		NULL				, 0 },	// real wan if; see wan.c:start_wan
 	{ "wan3_hilink_ip",		"0.0.0.0"			, 0 },
 	{ "wan3_status_script",		"0"				, 0 },
+#ifdef TCONFIG_BBT
 	{ "wan3_ckmtd",			"3"				, 0 },
+#else
+	{ "wan3_ckmtd",			"2"				, 0 },
+#endif
 
 	{ "wan4_proto",			"dhcp"				, 0 },	// [static|dhcp|pppoe|disabled]
 	{ "wan4_ipaddr",		"0.0.0.0"			, 0 },	// WAN IP address
@@ -112,8 +124,12 @@ struct nvram_tuple router_defaults[] = {
 	{ "wan4_ifnameX",		NULL				, 0 },	// real wan if; see wan.c:start_wan
 	{ "wan4_hilink_ip",		"0.0.0.0"			, 0 },
 	{ "wan4_status_script",		"0"				, 0 },
+#ifdef TCONFIG_BBT
 	{ "wan4_ckmtd",			"3"				, 0 },
+#else
+	{ "wan4_ckmtd",			"2"				, 0 },
 #endif
+#endif /* TCONFIG_MULTIWAN */
 
 #ifdef TCONFIG_DNSSEC
 	{ "dnssec_enable",		"0"				, 0 },
