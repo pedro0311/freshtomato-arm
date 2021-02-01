510c44ef8e7eda80272523c8515558997bee19ae
pedro <pedro311@gmail.com>
2021-02-01 19:18:18 +0100

    GUI: VPN Tunneling: PPTP Client: use ajax for Start/Stop button

--- release/src-rt-6.x.4708/router/www/isup.jsx
+++ release/src-rt-6.x.4708/router/www/isup.jsx
@@ -1,21 +1,25 @@
 isup = {};
 
-/* OPENVPN-BEGIN */
-isup.vpnserver1 = parseInt('<% psup("vpnserver1"); %>');
-isup.vpnserver2 = parseInt('<% psup("vpnserver2"); %>');
-/* OPENVPN-END */
-/* PPTPD-BEGIN */
-isup.pptpd = parseInt('<% psup("pptpd"); %>');
-/* PPTPD-END */
-/* NGINX-BEGIN */
-isup.nginx = parseInt('<% psup("nginx"); %>');
-/* NGINX-END */
+isup.telnetd = parseInt('<% psup("telnetd"); %>');
 
 /* it should be done in a different way, but for now it's ok */
 isup.qos = <% nv("qos_enable"); %>;
 isup.bwl = <% nv("bwl_enable"); %>;
 
-isup.telnetd = parseInt('<% psup("telnetd"); %>');
+/* OPENVPN-BEGIN */
+isup.vpnserver1 = parseInt('<% psup("vpnserver1"); %>');
+isup.vpnserver2 = parseInt('<% psup("vpnserver2"); %>');
+/* OPENVPN-END */
+
+/* PPTPD-BEGIN */
+isup.pptpclient = parseInt('<% psup("pptpclient"); %>');
+isup.pptpd = parseInt('<% psup("pptpd"); %>');
+/* PPTPD-END */
+
+/* NGINX-BEGIN */
+isup.nginx = parseInt('<% psup("nginx"); %>');
+/* NGINX-END */
+
 /* SSH-BEGIN */
 isup.dropbear = parseInt('<% psup("dropbear"); %>');
 /* SSH-END */
