9fcac880a68d97ca7ddb4390066647c98c9dbd0c
pedro <pedro311@gmail.com>
2022-06-25 22:18:08 +0200

    kernel sdk6: drivers: net: ppp_deflate: updates from the upstream
    ppp: deflate: never return len larger than output buffer
    https://github.com/torvalds/linux/commit/e2a4800e75780ccf4e6c2487f82b688ba736eb18
    
    ppp: deflate: Fix possible crash in deflate_init
    https://github.com/torvalds/linux/commit/3ebe1bca58c85325c97a22d4fc3f5b5420752e6f
    
    ppp: deflate: Remove useless call "zlib_inflateEnd"
    https://github.com/torvalds/linux/commit/dc5fa2073f63965faad232af0adf9b7e50b5f340

--- release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/ppp_deflate.c
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/drivers/net/ppp_deflate.c
@@ -268,7 +268,7 @@ static int z_compress(void *arg, unsigned char *rptr, unsigned char *obuf,
 	/*
 	 * See if we managed to reduce the size of the packet.
 	 */
-	if (olen < isize) {
+	if (olen < isize && olen <= osize) {
 		state->stats.comp_bytes += olen;
 		state->stats.comp_packets++;
 	} else {
@@ -304,7 +304,6 @@ static void z_decomp_free(void *arg)
 	struct ppp_deflate_state *state = (struct ppp_deflate_state *) arg;
 
 	if (state) {
-		zlib_inflateEnd(&state->strm);
 		vfree(state->strm.workspace);
 		kfree(state);
 	}
@@ -632,12 +631,20 @@ static struct compressor ppp_deflate_draft = {
 
 static int __init deflate_init(void)
 {
-        int answer = ppp_register_compressor(&ppp_deflate);
-        if (answer == 0)
-                printk(KERN_INFO
-		       "PPP Deflate Compression module registered\n");
-	ppp_register_compressor(&ppp_deflate_draft);
-        return answer;
+	int rc;
+
+	rc = ppp_register_compressor(&ppp_deflate);
+	if (rc)
+		return rc;
+
+	rc = ppp_register_compressor(&ppp_deflate_draft);
+	if (rc) {
+		ppp_unregister_compressor(&ppp_deflate);
+		return rc;
+	}
+
+	pr_info("PPP Deflate Compression module registered\n");
+	return 0;
 }
 
 static void __exit deflate_cleanup(void)
