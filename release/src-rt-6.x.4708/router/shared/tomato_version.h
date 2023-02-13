9985129b5784c8de7f75df3821da1881c5d93f6b
pedro <pedro311@gmail.com>
2023-02-13 13:27:46 +0100

    Bump version to 2023.1

--- release/src-rt-6.x.4708/router/shared/tomato_version.h
+++ release/src-rt-6.x.4708/router/shared/tomato_version.h
@@ -1,9 +1,9 @@
 #ifndef __TOMATO_VERSION_H__
 #define __TOMATO_VERSION_H__
-#define TOMATO_MAJOR		"2022"
-#define TOMATO_MINOR		"7"
-#define TOMATO_BUILD		"007"
+#define TOMATO_MAJOR		"2023"
+#define TOMATO_MINOR		"1"
+#define TOMATO_BUILD		"001"
 #define	TOMATO_BUILDTIME	"Thu, 26 Jan 2018 01:01:01 +0100"
-#define TOMATO_VERSION		"2022.7.001 -my_build- K26ARM USB AIO-64K"
-#define TOMATO_SHORTVER		"2022.7"
+#define TOMATO_VERSION		"2023.1.001 -my_build- K26ARM USB AIO-64K"
+#define TOMATO_SHORTVER		"2023.1"
 #endif
