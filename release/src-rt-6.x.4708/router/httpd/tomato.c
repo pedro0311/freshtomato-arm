fc55c3a125114369f24126ebc4cc7e3efba8d5d6
pedro <pedro311@gmail.com>
2023-07-09 15:07:35 +0200

    httpd: improve buffer validation (strlcpy)

--- release/src-rt-6.x.4708/router/httpd/tomato.c
+++ release/src-rt-6.x.4708/router/httpd/tomato.c
@@ -2,6 +2,8 @@
  * Tomato Firmware
  * Copyright (C) 2006-2010 Jonathan Zarate
  *
+ * Fixes/updates (C) 2018 - 2023 pedro
+ *
  */
 
 
@@ -1824,7 +1826,7 @@ static void _execute_command(char *url, char *command, char *query, wofilter_t w
 		}
 	}
 
-	if ((f = fdopen(fe, "wb")) != NULL) {
+	if ((f = fdopen(fe, "wb"))) {
 		fprintf(f,
 			"#!/bin/sh\n"
 			"export REQUEST_METHOD=\"%s\"\n"
@@ -1856,7 +1858,7 @@ static void _execute_command(char *url, char *command, char *query, wofilter_t w
 	chmod(webExecFile, 0700);
 
 	if (query) {
-		if ((f = fdopen(fq, "wb")) != NULL) {
+		if ((f = fdopen(fq, "wb"))) {
 			fprintf(f, "%s\n", query);
 			fclose(f);
 		}
@@ -2147,7 +2149,7 @@ static int nv_wl_bwcap_chanspec(int idx, int unit, int subunit, void *param)
 		return 1;
 
 	memset(chan_spec, 0, sizeof(chan_spec));
-	strncpy(chan_spec, ch, sizeof(chan_spec));
+	strlcpy(chan_spec, ch, sizeof(chan_spec));
 	switch (atoi(nbw_cap)) {
 		case 0:
 			if (write)
@@ -2163,7 +2165,7 @@ static int nv_wl_bwcap_chanspec(int idx, int unit, int subunit, void *param)
 			if (write)
 				nvram_set(wl_nvname("bw_cap", unit, 0), "7");
 			if (*ch != '0')
-				strcpy(chan_spec + strlen(chan_spec), "/80");
+				strlcpy(chan_spec + strlen(chan_spec), "/80", sizeof(chan_spec));
 			break;
 	}
 	if (write)
@@ -2231,7 +2233,7 @@ static int save_variables(int write)
 		if ((p = webcgi_get(s)) != NULL) {
 			if (strlen(p) > 2048) {
 				memset(t, 0, sizeof(t));
-				strncpy(t, s, sizeof(s));
+				strlcpy(t, s, sizeof(s));
 				snprintf(s, sizeof(s), msgf, t);
 				resmsg_set(s);
 				return 0;
