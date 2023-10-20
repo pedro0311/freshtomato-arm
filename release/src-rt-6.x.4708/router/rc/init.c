2ca90993da748a22b46dae5f211f7490f0935b8c
M_ars <M_ars@gmx.net>
2023-10-20 15:25:43 +0200

    BSD (wireless band steering): add bsd nvram variables only if feature is enabled (clean-up nvram at boot/re-boot)
    Note: save nvram space if feature is disabled.
    
    Note 2: manual bsd nvram clean-up also possible ("service bsd_nvram stop")

--- release/src-rt-6.x.4708/router/rc/init.c
+++ release/src-rt-6.x.4708/router/rc/init.c
@@ -5771,17 +5771,8 @@ static int init_nvram(void)
 			nvram_set("wl0_ssid", "FreshTomato50");
 			nvram_set("wl1_ssid", "FreshTomato24");
 
-#ifdef TCONFIG_BCMBSD
 			/* band steering settings correction, because 5 GHz module is the first one */
-			nvram_set("wl1_bsd_steering_policy", "0 5 3 -52 0 110 0x22");
-			nvram_set("wl0_bsd_steering_policy", "80 5 3 -82 0 0 0x20");
-			nvram_set("wl1_bsd_sta_select_policy", "10 -52 0 110 0 1 1 0 0 0 0x122");
-			nvram_set("wl0_bsd_sta_select_policy", "10 -82 0 0 0 1 1 0 0 0 0x20");
-			nvram_set("wl1_bsd_if_select_policy", "eth1");
-			nvram_set("wl0_bsd_if_select_policy", "eth2");
-			nvram_set("wl1_bsd_if_qualify_policy", "0 0x0");
-			nvram_set("wl0_bsd_if_qualify_policy", "60 0x0");
-#endif /* TCONFIG_BCMBSD */
+			/* see function start_bsd(void) */
 
 			/* usb settings */
 			nvram_set("usb_ohci", "1"); /* USB 1.1 */
@@ -6020,17 +6011,8 @@ static int init_nvram(void)
 			nvram_set("wl0_ssid", "FreshTomato50");
 			nvram_set("wl1_ssid", "FreshTomato24");
 
-#ifdef TCONFIG_BCMBSD
 			/* band steering settings correction, because 5 GHz module is the first one */
-			nvram_set("wl1_bsd_steering_policy", "0 5 3 -52 0 110 0x22");
-			nvram_set("wl0_bsd_steering_policy", "80 5 3 -82 0 0 0x20");
-			nvram_set("wl1_bsd_sta_select_policy", "10 -52 0 110 0 1 1 0 0 0 0x122");
-			nvram_set("wl0_bsd_sta_select_policy", "10 -82 0 0 0 1 1 0 0 0 0x20");
-			nvram_set("wl1_bsd_if_select_policy", "eth1");
-			nvram_set("wl0_bsd_if_select_policy", "eth2");
-			nvram_set("wl1_bsd_if_qualify_policy", "0 0x0");
-			nvram_set("wl0_bsd_if_qualify_policy", "60 0x0");
-#endif /* TCONFIG_BCMBSD */
+			/* see function start_bsd(void) */
 
 			/* usb settings */
 			nvram_set("usb_ohci", "1"); /* USB 1.1 */
@@ -8919,17 +8901,8 @@ static int init_nvram(void)
 			/* misc wifi settings */
 			nvram_set("wl1_vreqd", "0"); /* do not enable vhtmode and vht_features for 2G NON-AC PHY */
 
-#ifdef TCONFIG_BCMBSD
 			/* band steering settings correction, because 5 GHz module is the first one */
-			nvram_set("wl1_bsd_steering_policy", "0 5 3 -52 0 110 0x22");
-			nvram_set("wl0_bsd_steering_policy", "80 5 3 -82 0 0 0x20");
-			nvram_set("wl1_bsd_sta_select_policy", "10 -52 0 110 0 1 1 0 0 0 0x122");
-			nvram_set("wl0_bsd_sta_select_policy", "10 -82 0 0 0 1 1 0 0 0 0x20");
-			nvram_set("wl1_bsd_if_select_policy", "eth1");
-			nvram_set("wl0_bsd_if_select_policy", "eth2");
-			nvram_set("wl1_bsd_if_qualify_policy", "0 0x0");
-			nvram_set("wl0_bsd_if_qualify_policy", "60 0x0");
-#endif /* TCONFIG_BCMBSD */
+			/* see function start_bsd(void) */
 
 			/* usb settings */
 			nvram_set("usb_ohci", "1"); /* USB 1.1 */
@@ -9226,17 +9199,8 @@ static int init_nvram(void)
 				nvram_set("wl0_ssid", "FreshTomato50");
 				nvram_set("wl1_ssid", "FreshTomato24");
 
-#ifdef TCONFIG_BCMBSD
 				/* band steering settings correction, because 5 GHz module is the first one */
-				nvram_set("wl1_bsd_steering_policy", "0 5 3 -52 0 110 0x22");
-				nvram_set("wl0_bsd_steering_policy", "80 5 3 -82 0 0 0x20");
-				nvram_set("wl1_bsd_sta_select_policy", "10 -52 0 110 0 1 1 0 0 0 0x122");
-				nvram_set("wl0_bsd_sta_select_policy", "10 -82 0 0 0 1 1 0 0 0 0x20");
-				nvram_set("wl1_bsd_if_select_policy", "eth1");
-				nvram_set("wl0_bsd_if_select_policy", "eth2");
-				nvram_set("wl1_bsd_if_qualify_policy", "0 0x0");
-				nvram_set("wl0_bsd_if_qualify_policy", "60 0x0");
-#endif /* TCONFIG_BCMBSD */
+				/* see function start_bsd(void) */
 			}
 
 			/* 2.4 GHz and 5 GHz defaults */
@@ -11415,6 +11379,9 @@ static void sysinit(void)
 	del_snmp_defaults(); /* remove snmp nvram values if feature is disabled! */
 #endif /* TCONFIG_SNMP */
 	del_upnp_defaults(); /* remove upnp nvram values if feature is disabled! */
+#ifdef TCONFIG_BCMBSD
+	del_bsd_defaults(); /* remove BSD (smart connect / band steering) nvram values if feature is disabled! */
+#endif /* TCONFIG_BCMBSD */
 	init_nvram();
 
 	set_jumbo_frame(); /* enable or disable jumbo_frame and set jumbo frame size */
