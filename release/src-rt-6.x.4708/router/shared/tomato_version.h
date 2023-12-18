3c8befd9a7fa790737af37a77f0afcaa357a0291
pedro <pedro311@gmail.com>
2023-12-18 11:54:15 +0100

    Bump version to 2023.5

--- release/src-rt-6.x.4708/router/shared/tomato_version.h
+++ release/src-rt-6.x.4708/router/shared/tomato_version.h
@@ -1,7 +1,7 @@
 #ifndef __TOMATO_VERSION_H__
 #define __TOMATO_VERSION_H__
 #define TOMATO_MAJOR		"2023"
-#define TOMATO_MINOR		"4"
+#define TOMATO_MINOR		"5"
 #define TOMATO_BUILD		"1"
 #define	TOMATO_BUILDTIME	""
 #define TOMATO_VERSION		""
