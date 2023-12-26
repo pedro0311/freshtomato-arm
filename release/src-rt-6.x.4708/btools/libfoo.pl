e1aa6944c3c5365492255bf6093f9559c4cf8068
pedro <pedro311@gmail.com>
2023-12-26 14:23:44 +0100

    build: libxml2: fix build break

--- release/src-rt-6.x.4708/btools/libfoo.pl
+++ release/src-rt-6.x.4708/btools/libfoo.pl
@@ -262,7 +262,7 @@ sub fixDyn
 
 	fixDynDep("nginx", "libpcre.so.1.2.13");
 	fixDynDep("nginx", "libpcreposix.so.0.0.7");
-	fixDynDep("php-cgi", "libxml2.so.2.12.2");
+	fixDynDep("php-cgi", "libxml2.so.2.12.3");
 	fixDynDep("php-cgi", "libpng16.so.16.40.0");
 	fixDynDep("php-cgi", "libiconv.so.2.6.1");
 	fixDynDep("php-cgi", "libsqlite3.so.0.8.6");
@@ -270,7 +270,7 @@ sub fixDyn
 	fixDynDep("php-cgi", "libjpeg.so.8.3.2");
 	fixDynDep("php-cgi", "libpcre.so.1.2.13");
 	fixDynDep("php-cgi", "libpcreposix.so.0.0.7");
-	fixDynDep("php-cli", "libxml2.so.2.12.2");
+	fixDynDep("php-cli", "libxml2.so.2.12.3");
 	fixDynDep("php-cli", "libpng16.so.16.40.0");
 	fixDynDep("php-cli", "libiconv.so.2.6.1");
 	fixDynDep("php-cli", "libsqlite3.so.0.8.6");
@@ -632,7 +632,7 @@ genSO("${root}/usr/lib/libiconv.so.2.6.1", "${router}/libiconv/lib/.libs/libicon
 genSO("${root}/usr/lib/libnfnetlink.so.0.2.0", "${router}/libnfnetlink/src/.libs/libnfnetlink.a");
 genSO("${root}/usr/lib/libsodium.so.23.3.0", "${router}/libsodium/src/libsodium/.libs/libsodium.a");
 genSO("${root}/usr/lib/libpng16.so.16.40.0", "${router}/libpng/.libs/libpng16.a", "", "-L${router}/zlib");
-genSO("${root}/usr/lib/libxml2.so.2.12.2", "${router}/libxml2/.libs/libxml2.a", "", "-L${router}/zlib");
+genSO("${root}/usr/lib/libxml2.so.2.12.3", "${router}/libxml2/.libs/libxml2.a", "", "-L${router}/zlib");
 genSO("${root}/usr/lib/libpcre.so.1.2.13", "${router}/pcre/.libs/libpcre.a");
 genSO("${root}/usr/lib/libpcreposix.so.0.0.7", "${router}/pcre/.libs/libpcreposix.a");
 genSO("${root}/usr/lib/libatomic_ops.so.1.0.3", "${router}/libatomic_ops/src/.libs/libatomic_ops.a");
