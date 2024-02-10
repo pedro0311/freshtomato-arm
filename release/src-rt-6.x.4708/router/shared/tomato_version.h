0f19f68b34f7a55a7f8f0df8e1e06c7cd621b660
pedro <pedro311@gmail.com>
2024-02-10 18:30:49 +0100

    Bump version to 2024.1

--- release/src-rt-6.x.4708/router/shared/tomato_version.h
+++ release/src-rt-6.x.4708/router/shared/tomato_version.h
@@ -1,7 +1,7 @@
 #ifndef __TOMATO_VERSION_H__
 #define __TOMATO_VERSION_H__
-#define TOMATO_MAJOR		"2023"
-#define TOMATO_MINOR		"5"
+#define TOMATO_MAJOR		"2024"
+#define TOMATO_MINOR		"1"
 #define TOMATO_BUILD		"1"
 #define	TOMATO_BUILDTIME	""
 #define TOMATO_VERSION		""
