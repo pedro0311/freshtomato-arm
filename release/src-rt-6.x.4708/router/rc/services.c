2e37fca183db9e8a8edb4e21826cf424784399c5
pedro <pedro311@gmail.com>
2024-05-08 01:40:27 +0200

    ntpd: try to monitor and restart it when it dies or doesn't start at all

--- release/src-rt-6.x.4708/router/rc/services.c
+++ release/src-rt-6.x.4708/router/rc/services.c
@@ -99,6 +99,7 @@ static const char dmresolv[] = "/etc/resolv.dnsmasq";
 static pid_t pid_crond = -1;
 static pid_t pid_hotplug2 = -1;
 static pid_t pid_igmp = -1;
+static pid_t pid_ntpd = -1;
 #ifdef TCONFIG_FANCTRL
 static pid_t pid_phy_tempsense = -1;
 #endif
@@ -2494,6 +2495,10 @@ void start_ntpd(void)
 		}
 
 		ret = _eval(ntpd_argv, NULL, 0, &pid);
+
+		if (!nvram_contains_word("debug_norestart", "ntpd"))
+			pid_ntpd = -2;
+
 		if (ret)
 			logmsg(LOG_ERR, "starting ntpd failed ...");
 		else
@@ -2506,6 +2511,7 @@ void stop_ntpd(void)
 	if (serialize_restart("ntpd", 0))
 		return;
 
+	pid_ntpd = -1;
 	if (pidof("ntpd") > 0) {
 		killall_tk_period_wait("ntpd", 50);
 		logmsg(LOG_INFO, "ntpd is stopped");
@@ -2928,6 +2934,8 @@ void check_services(void)
 //		_check(pid_dnsmasq, "dnsmasq", start_dnsmasq);
 		_check(pid_crond, "crond", start_cron);
 		_check(pid_igmp, "igmpproxy", start_igmp_proxy);
+		if (nvram_get_int("ntp_updates") >= 1)
+			_check(pid_igmp, "ntpd", start_ntpd);
 	}
 }
 
