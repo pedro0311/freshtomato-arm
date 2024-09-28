721371dd6cf58ee4988dac41a24ddf28ef05e9d8
pedro <pedro311@gmail.com>
2024-09-28 16:46:36 +0200

    libpng: update to 1.6.44
    - switch to CMake in recipe

--- release/src-rt-6.x.4708/btools/libfoo.pl
+++ release/src-rt-6.x.4708/btools/libfoo.pl
@@ -268,7 +268,7 @@ sub fixDyn
 	fixDynDep("nginx", "libpcre2-posix.so.3.0.5");
 	fixDynDep("nginx", "libwolfssl.so.42.2.0");
 	fixDynDep("php-cgi", "libxml2.so.2.13.4");
-	fixDynDep("php-cgi", "libpng16.so.16.43.0");
+	fixDynDep("php-cgi", "libpng16.so.16.44.0");
 	fixDynDep("php-cgi", "libiconv.so.2.6.1");
 	fixDynDep("php-cgi", "libsqlite3.so.0.8.6");
 	fixDynDep("php-cgi", "libcurl.so.4.8.0");
@@ -279,7 +279,7 @@ sub fixDyn
 	fixDynDep("php-cgi", "libpcre2-posix.so.3.0.5");
 	fixDynDep("php-cgi", "libzip.so.5.5");
 	fixDynDep("php-cli", "libxml2.so.2.13.4");
-	fixDynDep("php-cli", "libpng16.so.16.43.0");
+	fixDynDep("php-cli", "libpng16.so.16.44.0");
 	fixDynDep("php-cli", "libiconv.so.2.6.1");
 	fixDynDep("php-cli", "libsqlite3.so.0.8.6");
 	fixDynDep("php-cli", "libcurl.so.4.8.0");
@@ -290,7 +290,7 @@ sub fixDyn
 	fixDynDep("php-cli", "libpcre2-posix.so.3.0.5");
 	fixDynDep("php-cli", "libzip.so.5.5");
 	fixDynDep("php-fpm", "libxml2.so.2.13.4");
-	fixDynDep("php-fpm", "libpng16.so.16.43.0");
+	fixDynDep("php-fpm", "libpng16.so.16.44.0");
 	fixDynDep("php-fpm", "libiconv.so.2.6.1");
 	fixDynDep("php-fpm", "libsqlite3.so.0.8.6");
 	fixDynDep("php-fpm", "libcurl.so.4.8.0");
@@ -690,7 +690,7 @@ genSO("${root}/usr/lib/liblz4.so.1.10.0", "${router}/lz4/lib/liblz4.a", "${strip
 genSO("${root}/usr/lib/libcurl.so.4.8.0", "${router}/libcurl/lib/.libs/libcurl.a", "${stripshared}", "-L${router}/zlib -L${router}/${openssldir} -L${router}/wolfssl/staged/usr/lib");
 genSO("${root}/usr/lib/libevent-2.1.so.7", "${router}/libevent/.libs/libevent.a", "${stripshared}");
 genSO("${root}/usr/lib/libiconv.so.2.6.1", "${router}/libiconv/lib/.libs/libiconv.a", "${stripshared}");
-genSO("${root}/usr/lib/libpng16.so.16.43.0", "${router}/libpng/.libs/libpng16.a", "${stripshared}", "-L${router}/zlib");
+genSO("${root}/usr/lib/libpng16.so.16.44.0", "${router}/libpng/.libs/libpng16.a", "${stripshared}", "-L${router}/zlib");
 genSO("${root}/usr/lib/libxml2.so.2.13.4", "${router}/libxml2/staged/usr/lib/libxml2.a", "${stripshared}", "-L${router}/zlib");
 genSO("${root}/usr/lib/libpcre.so.1.2.13", "${router}/pcre/.libs/libpcre.a", "${stripshared}");
 genSO("${root}/usr/lib/libpcre2-8.so.0.13.0", "${router}/pcre2/staged/usr/lib/libpcre2-8.a", "${stripshared}");
